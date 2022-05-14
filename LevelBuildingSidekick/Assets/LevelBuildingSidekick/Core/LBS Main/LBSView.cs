5cbcb52c59212c5ea924f33faafdd62ac2652934
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-14 11:44:36 -0400

    Rename

---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/LBSView.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class LBSView: EditorWindow
+{
+    static string header;
+
+    private void OnEnable()
+    {
+        
+    }
+
+
+    private void OnGUI()
+    {
+        
+    }
+}
