9a745558d91f81fb78be5372da0f3ec2580efc59
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-14 11:50:43 -0400

    Step class
    -Abstract class for inheritance. Extends from Controller.

---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/Step.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LevelBuildingSidekick
+{
+    public abstract class Step: Controller
+    {
+        LevelRepresentationController levelRepresentation;
+    }
+}
+
