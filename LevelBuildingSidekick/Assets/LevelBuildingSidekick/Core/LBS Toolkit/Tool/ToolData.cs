1337647baebc9ba053869de28bf85e4a65247f0e
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-14 11:52:37 -0400

    MVC structure and folder for tool
    - Abstract classes for inheritance

---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Toolkit/Tool/ToolData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LevelBuildingSidekick;
+
+public class ToolData : Data
+{
+}
---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Toolkit/Tool/ToolView.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LevelBuildingSidekick;
+
+public abstract class ToolView : View
+{
+
+}
