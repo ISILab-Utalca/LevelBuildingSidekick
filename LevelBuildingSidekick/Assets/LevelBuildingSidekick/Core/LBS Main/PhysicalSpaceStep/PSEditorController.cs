129481b1ab89c688ac2a9dd8a081be151199e941
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-14 11:51:21 -0400

    MVC structure for Physical Space Step

---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/PhysicalSpaceStep/PSEditorController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelBuildingSidekick
+{
+    public class PSEditorController : Step
+    {
+        GraphController graph;
+        public override void Update()
+        {
+        }
+    }
+}
+
---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/PhysicalSpaceStep/PSEditorView.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelBuildingSidekick
+{
+    public class PSEditorView : View
+    {
+        public override void Draw(Rect rect)
+        {
+        }
+    }
+}
+
