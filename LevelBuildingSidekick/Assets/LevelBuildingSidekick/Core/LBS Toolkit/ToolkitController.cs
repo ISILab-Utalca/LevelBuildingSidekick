2db15316687b4d0d49f4f95b9f4a1992efaa0cc0
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-14 11:52:09 -0400

    MVC structure and folder for Toolkit

---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Toolkit/ToolkitController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LevelBuildingSidekick;
+
+public class ToolkitController : Controller
+{
+    public override void Update()
+    {
+    }
+}
---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Toolkit/ToolkitView.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LevelBuildingSidekick;
+
+public class ToolkitView : View
+{
+    public override void Draw(Rect rect)
+    {
+    }
+}
