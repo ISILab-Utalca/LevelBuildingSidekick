4615065658d6c116196f98b0e80460643b4e39ec
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-16 13:31:53 -0400

    Update PSEditorView.cs
    Display method from inheritance
    PSEWindow

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/PhysicalSpaceStep/PSEditorView.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/PhysicalSpaceStep/PSEditorView.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace LevelBuildingSidekick
 {
     public class PSEditorView : View
     {
+        public override void Display()
+        {
+            throw new System.NotImplementedException();
+        }
+
         public override void Draw(Rect rect)
         {
         }
+    }
+
+    public class PSEWindow: EditorWindow
+    {
+
     }
 }
 
