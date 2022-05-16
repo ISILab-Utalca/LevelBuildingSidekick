7ab0a853376dc2dc88d8c6cb4d11ff08c411cb3e
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-16 17:38:24 -0400

    Update LBSController.cs
    Support any type of Step and adds it to the tool and displays the view on the main window.

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/LBSController.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/LBSController.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 namespace LevelBuildingSidekick
 {
     public class LBSController : Controller
     {
-        public static LBSController Instance{
+        public static LBSController Instance {
             get
             {
-                if(_Instance == null)
+                if (_Instance == null)
                 {
                     _Instance = new LBSController();
                 }
@@ -19,18 +20,21 @@ namespace LevelBuildingSidekick
         }
         private static LBSController _Instance;
 
-        static List<Step> steps;
-        static Step currentStep;
+        List<Step> steps;
+        public Step currentStep;
 
         public LBSController()
         {
-            View = new LBSView();
+            View = new LBSView(this);
+            steps = new List<Step>();
+
+            LoadData();
         }
 
         [MenuItem("Level Building Sidekick/Open New")]
         public static void ShowWindow()
         {
-            Instance.View.Display();
+            Instance.currentStep.View.Display();
             //view = EditorWindow.GetWindow<LBSWindow>("Level Building Sidekick");
             //view.Show();
             /*
@@ -42,8 +46,48 @@ namespace LevelBuildingSidekick
 
         public override void LoadData()
         {
-            LBSData data = Data as LBSData;
-            throw new System.NotImplementedException();
+            LBSData data = new LBSData();
+            //Parse data from Data to LBSData
+            if(Data == null)
+            {
+                try
+                {
+                    Data = Resources.Load("New LBS Data") as LBSData;
+                }
+                catch
+                {
+                    Debug.LogError("No Data Found");
+                }
+            }
+
+            try
+            {
+                data = Data as LBSData;
+            }
+            catch
+            {
+                Debug.LogError("Incorrect Data Type");
+                return;
+            }
+
+            foreach (Data d in data.StepsData)
+            {
+                var step = Activator.CreateInstance(d.ControllerType);
+                if(step is Step)
+                {
+                    (step as Step).Data = d;
+                    Debug.Log(d.GetType());
+                    steps.Add(step as Step);
+                    continue;
+                }
+                Debug.LogError("Type: " + d.ControllerType + " is not an inheritance of Step class");
+            }
+
+            Debug.Log(steps.Count);
+            if (steps.Count > 0)
+            {
+                currentStep = steps[0];
+            }
         }
 
         public override void Update()
