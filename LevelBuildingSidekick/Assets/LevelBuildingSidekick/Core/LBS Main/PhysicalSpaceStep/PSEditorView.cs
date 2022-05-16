5a519d7fc86aaa80640adcb1b9dcada627f8f88a
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-16 17:35:55 -0400

    Update PSEditorView.cs
    View/Window architecture
    Window calls View Draw method OnGUI through Event
    View show Window in Display
    
    There are Views that are panels so in Display they have to directly call Draw

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/PhysicalSpaceStep/PSEditorView.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/PhysicalSpaceStep/PSEditorView.cs
@@ -7,19 +7,44 @@ namespace LevelBuildingSidekick
 {
     public class PSEditorView : View
     {
-        public override void Display()
+        PSEWindow Window { get; set; }
+        Vector2 scrollPosition;
+
+        public PSEditorView(Controller controller):base(controller)
         {
-            throw new System.NotImplementedException();
+            scrollPosition = Vector2.zero;
+            Window = new PSEWindow();
+            Window.draw = Draw;
+            //Debug.Log("D: " + (Controller.Data as PSEditorData));
+
         }
 
-        public override void Draw(Rect rect)
+        public override void Display()
         {
+            Window.titleContent.text = (Controller.Data as PSEditorData).WindowName;
+            Window.Show();
+        }
+
+        public override void Draw()
+        {
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+            (Controller as PSEditorController).level.View.Display();
+            EditorGUILayout.EndScrollView();
         }
     }
 
     public class PSEWindow: EditorWindow
     {
+        public System.Action draw;
+        private void OnEnable()
+        {
+            
+        }
 
+        private void OnGUI()
+        {
+            draw?.Invoke();
+        }
     }
 }
 
