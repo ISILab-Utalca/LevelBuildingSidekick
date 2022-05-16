bcd7baee9a99bb9249c82497a371a5abdcbbd8b9
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-16 13:28:35 -0400

    Update LBSData.cs
    LevelBuildingSidekick Namespace

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/LBSData.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/LBSData.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LBSData : MonoBehaviour
+namespace LevelBuildingSidekick
 {
-    // Start is called before the first frame update
-    void Start()
+    [CreateAssetMenu(menuName = "LevelBuildingSidekick/GlobalData")]
+    public class LBSData : Data
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        List<Data> stepsData;
     }
 }
+
