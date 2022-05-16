28a1cbcb6b0e005aa424ae306894e42857097922
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-16 17:24:07 -0400

    Update Data.cs

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/MVC/Data.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/MVC/Data.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace LevelBuildingSidekick 
 {
+    [System.Serializable]
     public abstract class Data : ScriptableObject
     {
+        public abstract Type ControllerType { get; }
     }
 
 }
