c27e410a5f3e9f2bcc7e03b2e10bbb19173b9905
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-16 17:34:06 -0400

    Update PSEditorData.cs
    Controllertype
    Window Name

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/PhysicalSpaceStep/PSEditorData.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/PhysicalSpaceStep/PSEditorData.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace LevelBuildingSidekick
 {
     [CreateAssetMenu(menuName = "LevelBuildingSidekick/PhysicalSpaceData")]
     public class PSEditorData : Data
     {
-            
+        public string WindowName;
+        public override Type ControllerType => typeof(PSEditorController);
+
     }
 }
 
