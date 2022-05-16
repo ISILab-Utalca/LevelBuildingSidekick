464b9c015171f588a21067bc34c2d1cf1dbd0bb8
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-16 13:33:17 -0400

    Update LBSController.cs
    LevelBuildingSidekick namespace
    LoadData method from inheritance
    LBSController Singleton

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/LBSController.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Main/LBSController.cs
@@ -3,15 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class LBSController
+namespace LevelBuildingSidekick
 {
-    static EditorWindow view;
-    LBSData data;
-
-    [MenuItem("Level Building Sidekick/Open New")]
-    public static void ShowWindow()
+    public class LBSController : Controller
     {
-        view = EditorWindow.GetWindow<EditorWindow>("Level Building Sidekick");
-        view.Show();
+        public static LBSController Instance{
+            get
+            {
+                if(_Instance == null)
+                {
+                    _Instance = new LBSController();
+                }
+                return _Instance;
+            }
+        }
+        private static LBSController _Instance;
+
+        static List<Step> steps;
+        static Step currentStep;
+
+        public LBSController()
+        {
+            View = new LBSView();
+        }
+
+        [MenuItem("Level Building Sidekick/Open New")]
+        public static void ShowWindow()
+        {
+            Instance.View.Display();
+            //view = EditorWindow.GetWindow<LBSWindow>("Level Building Sidekick");
+            //view.Show();
+            /*
+            if(currentStep != null)
+            {
+                view.titleContent.text = currentStep.Name;
+            }*/
+        }
+
+        public override void LoadData()
+        {
+            LBSData data = Data as LBSData;
+            throw new System.NotImplementedException();
+        }
+
+        public override void Update()
+        {
+        }
     }
 }
+
+
