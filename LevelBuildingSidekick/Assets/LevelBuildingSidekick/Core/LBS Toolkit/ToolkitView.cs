01783efced33123dba2b406c6a33e765769c6cc7
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-18 16:41:57 -0400

    Update ToolkitView.cs

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Toolkit/ToolkitView.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Toolkit/ToolkitView.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using LevelBuildingSidekick;
+using UnityEditor.Overlays;
+using UnityEngine.UIElements;
+using UnityEditor;
 
 public class ToolkitView : View
 {
+
     public ToolkitView(Controller controller) : base(controller)
     {
+        ToolkitOverlay.draw = Draw;
     }
 
     public override void Display()
     {
-        throw new System.NotImplementedException();
+        //Draw();
     }
 
     public override void Draw()
     {
+        //GUILayout.Label("Toolkit");
+        var controller = Controller as ToolkitController;
+        foreach(ToolController t in controller.ToolControllers)
+        {
+            t.View.Display();
+        }
     }
+
 }
