a558da936d9e676d7bdf58f86b5f3b5f5e4281cd
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-22 03:15:18 -0400

    Update NodeData.cs
    Added basic properties

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeData.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeData.cs
@@ -9,9 +9,12 @@ using System;
 public class NodeData : Data
 {
     public string label;
+    public Vector2Int width;
+    public Vector2Int height;
+    public Vector2Int aspectRatio;
 
     public Vector2 Position { get; set ; }
-    public float Radius { get => 80;} // -> static?
+    public float Radius { get => 64;} // -> static?
     public Texture2D Sprite { get => Resources.Load("Textures/Circle") as Texture2D;} // -> static?
 
 
