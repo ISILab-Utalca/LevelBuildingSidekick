d76307c88caa8f6bdd963984c923ed0f378e50de
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-05-22 03:17:03 -0400

    Select Node tool
    MVC structure

---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Tools/SelectNode/SelectNodeController.cs
@@ -0,0 +1,44 @@
+using LevelBuildingSidekick;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectNodeController : ToolController
+{
+    bool waiting;
+    //public static NodeController SelectedNode { get; set; }
+    public SelectNodeController(Data data, ToolkitController toolkit) : base(data, toolkit)
+    {
+        View = new SelectNodeView(this);
+    }
+
+    public override void Action(LevelRepresentationController level)
+    {
+        GraphController graph = level as GraphController;
+
+        NodeController n = graph.GetNodeAt(Event.current.mousePosition);
+        if(n!=null)
+        {
+            graph.SelectedNode = n;
+        }
+    }
+
+    public override void LoadData()
+    {
+    }
+
+    public override void Update()
+    {
+        Event e = Event.current;
+        if (e.button == 0 && e.type.Equals(EventType.MouseDown))
+        {
+            //Debug.Log("Down");
+            waiting = true;
+        }
+        if (waiting && (e.button == 0 && e.type.Equals(EventType.MouseUp)))
+        {
+            Action(Toolkit.Level);
+            waiting = false;
+        }
+    }
+}
---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Tools/SelectNode/SelectNodeView.cs
@@ -0,0 +1,19 @@
+using LevelBuildingSidekick;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectNodeView : ToolView
+{
+    public SelectNodeView(Controller controller) : base(controller)
+    {
+    }
+
+    public override void Display()
+    {
+    }
+
+    public override void Draw()
+    {
+    }
+}
