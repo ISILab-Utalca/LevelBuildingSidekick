bc428b8771ed6baabbaedbd087e1d014889a5a45
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-06-06 10:00:13 -0400

    Position as Vector2Int instead of Vector2

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeController.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeController.cs
@@ -3,31 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using LevelBuildingSidekick;
 
-public class NodeController : Controller
+namespace LevelBuildingSidekick.Graph
 {
-    public NodeController(Data data) : base(data)
+    public class NodeController : Controller
     {
-        View = new NodeView(this);
-    }
+        public List<NodeController> neighbors;
+        public Vector2Int Position => (Data as NodeData).Position;
+        public NodeController(Data data) : base(data)
+        {
+            View = new NodeView(this);
+            neighbors = new List<NodeController>();
+        }
 
-    public override void LoadData()
-    {
-        //throw new System.NotImplementedException();
-    }
+        public override void LoadData()
+        {
+            //throw new System.NotImplementedException();
+        }
 
-    public override void Update()
-    {
-    }
+        public override void Update()
+        {
+        }
 
-    public Rect GetRect()
-    {
-        NodeData d = Data as NodeData;
-        return new Rect(d.Position, d.Radius * 2 * Vector2.one);
-    }
+        public Rect GetRect()
+        {
+            NodeData d = Data as NodeData;
+            return new Rect(d.Position, d.Radius * 2 * Vector2.one);
+        }
 
-    public void Translate(Vector2 delta)
-    {
-        NodeData d = Data as NodeData;
-        d.Position += delta;
+        public void Translate(Vector2 delta)
+        {
+            NodeData d = Data as NodeData;
+            d.Position += new Vector2Int((int)delta.x,(int)delta.y);
+        }
+        public void Translate(Vector2Int delta)
+        {
+            NodeData d = Data as NodeData;
+            d.Position += delta;
+        }
     }
 }
+
--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeData.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeData.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditorInternal;
 using LevelBuildingSidekick;
 using System;
 
-[System.Serializable]
-[CreateAssetMenu(menuName = ("LevelBuildingSidekick/Level Represetations/Graph Representation/Node"))]
-public class NodeData : Data
+namespace LevelBuildingSidekick.Graph
 {
-    public string label;
-    public Vector2Int width;
-    public Vector2Int height;
-    public Vector2Int aspectRatio;
+    [System.Serializable]
+    [CreateAssetMenu(menuName = ("LevelBuildingSidekick/Level Represetations/Graph Representation/Node"))]
+    public class NodeData : Data
+    {
+        public string label;
+        public Vector2Int width;
+        public Vector2Int height;
+        public Vector2Int aspectRatio;
 
-    public Vector2 Position { get; set ; }
-    public float Radius { get => 64;} // -> static?
-    public Texture2D Sprite { get => Resources.Load("Textures/Circle") as Texture2D;} // -> static?
+        //Should be in children class
+        [NonReorderable]
+        public List<GameObject> floorTiles;
+        //[SerializeField]
+        [NonReorderable]
+        public List<GameObject> wallTiles;
+        //[SerializeField]
+        [NonReorderable]
+        public List<GameObject> doorTiles;
+
+        public List<string> tags;
+
+        public Vector2Int Position { get; set; }
+        public float Radius { get => 64; } // -> static?
+        public Texture2D Sprite { get => Resources.Load("Textures/Circle") as Texture2D; } // -> static?
 
 
-    public override Type ControllerType => typeof(NodeController);
+        public override Type ControllerType => typeof(NodeController);
+
+    }
 }
+
