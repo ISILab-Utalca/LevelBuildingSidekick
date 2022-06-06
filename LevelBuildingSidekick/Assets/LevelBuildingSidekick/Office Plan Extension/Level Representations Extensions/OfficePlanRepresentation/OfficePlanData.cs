57fded0ca530cf301515ca531415ed41f0ffad92
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-06-06 10:03:22 -0400

    Update OfficePlanData.cs

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/OfficePlanRepresentation/OfficePlanData.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/OfficePlanRepresentation/OfficePlanData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using LevelBuildingSidekick;
 using System;
+using LevelBuildingSidekick.Graph;
 
 public class OfficePlanData : LevelRepresentationData
 {
