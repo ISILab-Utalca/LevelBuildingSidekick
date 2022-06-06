bc824345ff1fd4d5598f9d7b8556f5ffa15541ab
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-06-06 12:22:13 -0400

    Update NodeEditor.cs

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeEditor.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeEditor.cs
@@ -36,58 +36,7 @@ namespace LevelBuildingSidekick.Graph
         // Update is called once per frame
         public override void OnInspectorGUI()
         {
-            NodeData d = target as NodeData;
-            d.name = EditorGUILayout.TextField("Name: ", d.name);
-            d.width = EditorGUILayout.Vector2IntField("Width: ", d.width);
-            d.height = EditorGUILayout.Vector2IntField("Height: ", d.height);
-            d.aspectRatio = EditorGUILayout.Vector2IntField("Aspect Ratio: ", d.aspectRatio);
-
-            floorTiles = new ReorderableList(d.floorTiles, typeof(List<GameObject>), true, true, true, true)
-            {
-                drawHeaderCallback = (rect) =>
-                {
-                    GUI.Label(rect, "Floor Tiles");
-                },
-                drawElementCallback = (rect, index, active, focused) =>
-                {
-                }
-
-            };
-            floorTiles.DoLayoutList();
-
-            wallTiles = new ReorderableList(d.wallTiles, typeof(List<GameObject>), true, true, true, true)
-            {
-                drawHeaderCallback = (rect) =>
-                {
-                    GUI.Label(rect, "Wall Tiles");
-                },
-                drawElementCallback = (rect, index, active, focused) =>
-                {
-                    Type t = d.wallTiles.GetType();
-                    SerializedProperty element = serializedObject.FindProperty("wallTiles").GetArrayElementAtIndex(index);
-                    EditorGUI.ObjectField(rect, element, t, new GUIContent("Element " + index));
-                    d.wallTiles[index] = element.objectReferenceValue as GameObject;
-                },
-            };
-            wallTiles.DoLayoutList();
-
-            doorTiles = new ReorderableList(d.doorTiles, typeof(List<GameObject>), true, true, true, true)
-            {
-                drawHeaderCallback = (rect) =>
-                {
-                    GUI.Label(rect, "Door Tiles");
-                },
-                drawElementCallback = (rect, index, active, focused) =>
-                {
-                    Type t = d.doorTiles.GetType();
-                    SerializedProperty element = serializedObject.FindProperty("doorTiles").GetArrayElementAtIndex(index);
-                    EditorGUI.ObjectField(rect, element, t, new GUIContent("Element " + index));
-                    d.doorTiles[index] = element.objectReferenceValue as GameObject;
-                },
-            };
-            doorTiles.DoLayoutList();
-
-            //base.OnInspectorGUI();
+           base.OnInspectorGUI();
         }
     }
 }
