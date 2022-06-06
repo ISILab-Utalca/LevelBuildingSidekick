e24a0d2df3c60aafaac9f8ef00e8d1344e7beb07
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-06-06 09:59:13 -0400

    Update GraphView.cs
    ScreollView

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/GraphView.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/GraphView.cs
@@ -3,52 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using LevelBuildingSidekick;
+using System;
 
-public class GraphView : LevelRepresentationView
+namespace LevelBuildingSidekick.Graph
 {
-    NodeWindow nodeInspector;
-    Vector2 scrollPos;
-    public GraphView(Controller controller) : base(controller)
+    public class GraphView : LevelRepresentationView
     {
-    }
-
-    public override void Draw()
-    {
-        base.Draw();
-        //Debug.Log("Graph View");
-        //scrollPos = EditorGUILayout.BeginScrollView(scrollPos,true,true);
-        //GUILayout.BeginArea(new Rect(0, 0, 1000, 1000));
-        var controller = Controller as GraphController;
-
-        if(controller.SelectedNode != null)
+        NodeWindow nodeInspector;
+        Vector2 scrollPos;
+        Vector2 lastElement;
+        Vector2 offset;
+        public GraphView(Controller controller) : base(controller)
         {
-            if(nodeInspector == null)
+            offset = new Vector2Int(256, 256);
+        }
+
+        public override void Draw()
+        {
+            var controller = Controller as GraphController;
+
+            scrollPos = GUILayout.BeginScrollView(scrollPos);
+
+            lastElement = controller.LastNode();
+            var size = lastElement + offset;
+
+            var r = GUILayoutUtility.GetRect(size.x, size.y);
+            GUI.DrawTexture(r, new Texture2D(1, 1));
+            base.Draw();
+            //Debug.Log("Graph View");
+
+            if (controller.SelectedNode != null)
             {
-                nodeInspector = EditorWindow.GetWindow<NodeWindow>();
+                if (nodeInspector == null)
+                {
+                    nodeInspector = EditorWindow.GetWindow<NodeWindow>(new Type[] { typeof(StepWindow) });
+                }
+                nodeInspector.controller = controller.SelectedNode;
+                /*if(nodeInspector.Data == null)// || !nodeInspector.Data.Equals(controller.SelectedNode.Data))
+                {
+                    nodeInspector.Data = controller.SelectedNode.Data as NodeData;
+                }*/
             }
-            if(nodeInspector.Data == null || !nodeInspector.Data.Equals(controller.SelectedNode.Data))
+
+
+            foreach (NodeController n in controller.Nodes)
             {
-                nodeInspector.Data = controller.SelectedNode.Data as NodeData;
+                n.View.Display();
             }
-        }
+            foreach (EdgeController e in controller.Edges)
+            {
+                e.View.Display();
+            }
+            GUILayout.EndScrollView();
 
-
-        foreach(NodeController n in controller.Nodes)
-        {
-            n.View.Display();
         }
-        foreach(EdgeController e in controller.Edges)
-        {
-            e.View.Display();
-        }
-        /*
-        for(int i = 0; i < 100; i++)
-        {
-            GUILayout.Label(i.ToString());
-        }*/
-
-        //GUILayout.EndArea();
-        //EditorGUILayout.EndScrollView();
     }
 }
+
 
