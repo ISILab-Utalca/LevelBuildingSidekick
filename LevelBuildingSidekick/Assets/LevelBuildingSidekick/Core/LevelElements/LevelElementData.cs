bdb26ca17567bc8339613c5964b65e03f2730369
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-06-06 09:41:27 -0400

    LevelElement

---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LevelElements/LevelElementData.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelBuildingSidekick
+{
+    [CreateAssetMenu(menuName = "LevelBuildingSidekick/Level Element")]
+    [System.Serializable]
+    public class LevelElementData : Data
+    {
+        public override Type ControllerType => typeof(LevelElementController);
+        List<string> tags;
+       
+    }
+}
+
