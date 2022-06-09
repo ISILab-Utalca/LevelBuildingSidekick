4ac6ee925fb5f030b295eea1c940c5dd820b7a8f
Irith14 <44303941+Irith14@users.noreply.github.com>
2022-06-09 09:01:04 -0400

    Editor para clases node data

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeEditor.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Node/NodeEditor.cs
@@ -7,6 +7,7 @@ using UnityEditorInternal;
 
 namespace LevelBuildingSidekick.Graph
 {
+    [CustomEditor(typeof(NodeData))]
     public class NodeEditor : Editor
     {
         SerializedProperty node;
@@ -36,8 +37,49 @@ namespace LevelBuildingSidekick.Graph
         // Update is called once per frame
         public override void OnInspectorGUI()
         {
-           base.OnInspectorGUI();
+            //base.OnInspectorGUI();
+            NodeData g = target as NodeData;
+            //Espacio para proximo control
+            EditorGUILayout.Space();
+            
+            g.label = EditorGUILayout.TextField("Label ", g.label);
+           
+            //Espacio para proximo control
+            EditorGUILayout.Space();
+            g.proportionType = (ProportionType)EditorGUILayout.EnumPopup("Proportion type", g.proportionType);
+
+            switch (g.proportionType)
+            {
+                case ProportionType.RATIO:
+                    g.aspectRatio = EditorGUILayout.Vector2IntField("Aspect Radio ", g.aspectRatio);
+                    break;
+                case ProportionType.SIZE:
+                    g.width = EditorGUILayout.Vector2IntField("Width ", g.width);
+                    g.height = EditorGUILayout.Vector2IntField("Height", g.height);
+                    break;
+            }
+
+            //Espacio para proximo control
+            EditorGUILayout.Space();
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.Space();
+            var listFloor = g.floorTiles;
+            int newCount = Mathf.Max(0, EditorGUILayout.IntField("Size", listFloor.Count));
+            while (newCount < listFloor.Count)
+                listFloor.RemoveAt(listFloor.Count - 1);
+            while (newCount > listFloor.Count)
+                listFloor.Add(null);
+
+            for (int i = 0; i < listFloor.Count; i++)
+            {
+                //listFloor[i] = EditorGUILayout.TextField("Element: " + i, listFloor[i]);
+            }
+            EditorGUILayout.EndHorizontal();
+
         }
+
+       
+        
     }
 }
 
