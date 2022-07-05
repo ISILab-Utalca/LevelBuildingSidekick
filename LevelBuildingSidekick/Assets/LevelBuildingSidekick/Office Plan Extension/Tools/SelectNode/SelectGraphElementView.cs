394bac7a8fa895721e9041c4cfce8bf1a5088672
Irith14 <44303941+Irith14@users.noreply.github.com>
2022-07-05 17:45:13 -0400

    icon select
    Arroja null reference

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Tools/SelectNode/SelectGraphElementView.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Tools/SelectNode/SelectGraphElementView.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 
 public class SelectGraphElementView : ToolView
 {
+    Texture2D active;
+    Texture2D unnactive;
     public SelectGraphElementView(Controller controller) : base(controller)
     {
+        active = Resources.Load("Icons/Icon2_Select") as Texture2D;
+        unnactive = Resources.Load("Icons/Icon1_Select") as Texture2D;
     }
 
     public override void DrawInToolkit()
     {
+        var data = Controller.Data as DeleteGraphElementData;
+        var controller = Controller as DeleteGraphElementController;
+        
+        var t = controller.IsActive ? active : unnactive;
+        if (GUILayout.Button(t))
+        {
+            controller.Switch();
+        }
     }
 
     public override void Draw2D()
