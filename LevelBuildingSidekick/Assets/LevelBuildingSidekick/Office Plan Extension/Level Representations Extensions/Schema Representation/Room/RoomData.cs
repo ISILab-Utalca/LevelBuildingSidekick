c748713f884a7d8eaa6aa0a5cbf50353d53a5e07
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-07-26 14:48:26 -0400

    Clone Method in Data

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Schema Representation/Room/RoomData.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Schema Representation/Room/RoomData.cs
@@ -11,14 +11,23 @@ namespace LevelBuildingSidekick.Blueprint
 
         public Vector2Int position = Vector2Int.zero;
         //public Vector2Int bounds = Vector2Int.one;
-        public Vector2Int outerBounds = Vector2Int.one;
+        //public Vector2Int outerBounds = Vector2Int.one;
         public HashSet<Vector2Int> tilePositions = new HashSet<Vector2Int>();
-        public int[,] surface = new int[1,1];
 
 
         public override Type ControllerType => typeof(RoomController);
 
-
+        public RoomData Clone()
+        {
+            var r = new RoomData();
+            r.room = room;
+            r.position = position;
+            foreach(Vector2Int v in tilePositions)
+            {
+                r.tilePositions.Add(v);
+            }
+            return r;
+        }
     }
 }
 
--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Schema Representation/SchemaData.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Schema Representation/SchemaData.cs
@@ -14,11 +14,20 @@ namespace LevelBuildingSidekick.Blueprint
         public Vector2Int size;
         public List<RoomData> rooms;
         //public Dictionary<int, HashSet<int>> connections;
-
-
-
         public override Type ControllerType => typeof(SchemaController);
 
+        public SchemaData Clone()
+        {
+            var schema = new SchemaData();
+            schema.tileSize = tileSize;
+            schema.size = size;
+            schema.rooms = new List<RoomData>();
+            foreach(RoomData r in rooms)
+            {
+                schema.rooms.Add(r.Clone());
+            }
+            return schema;
+        }
     }
 }
 
