6156ff4fdbb44758881628b8018841f4883b2515
pokelocos <nare.rare.fire@gmail.com>
2022-08-02 08:40:05 -0400

    Update ToolController.cs

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Toolkit/Tool/ToolController.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Core/LBS Toolkit/Tool/ToolController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Events;
 using LevelBuildingSidekick;
+using UnityEngine;
 
 public abstract class ToolController : Controller
 {
+    public Vector2 CurrentPos { get; set; } // podria ser una variable solo de metodo y no global
     protected ToolkitController Toolkit { get; set; }
     public bool IsActive { get; set; }
     protected ToolController(Data data, ToolkitController toolkit) : base(data)
@@ -22,9 +24,32 @@ public abstract class ToolController : Controller
 
     public abstract void Action(LevelRepresentationController level);
 
-    public override void Update()
+    internal void InternalUpdate()
     {
+        if (IsActive)
+        {
+            Event e = Event.current;
+            CurrentPos = e.mousePosition;
+            if (e.button == 0 && e.type.Equals(EventType.MouseDown))
+            {
+                OnMouseDown(CurrentPos);
+            }
+            if (e.button == 0 && e.type.Equals(EventType.MouseDrag))
+            {
+                OnMouseDrag(CurrentPos);
+            }
+            if (e.button == 0 && e.type.Equals(EventType.MouseUp))
+            {
+                OnMouseUp(CurrentPos);
+            }
+        }
     }
 
+    public override void Update() { }
 
+    public virtual void OnMouseDown(Vector2 position) { }
+    public virtual void OnMouseUp(Vector2 position) { }
+    public virtual void OnMouseDrag(Vector2 position) { }
+
+    
 }
