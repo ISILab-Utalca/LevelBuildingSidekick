e875b235932088537c313f1469c034d4bf3507e6
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-08-04 09:27:44 -0400

    Update EdgeData.cs
    edge no stores id instead of reference

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Edge/EdgeData.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Office Plan Extension/Level Representations Extensions/Graph Representation/Edge/EdgeData.cs
@@ -9,10 +9,20 @@ using Newtonsoft.Json;
 [System.Serializable]
 public class EdgeData: Data
 {
-    public NodeData node1; //Should be ID (!!!)
-    public NodeData node2; //Should be ID (!!!)
+    public int firstNodeID; //Should be ID (!!!)
+    public int secondNodeID; //Should be ID (!!!)
+
+    [JsonIgnore]
     public float thikness = 5; // -> static (??)
 
     [JsonIgnore]
     public override Type ControllerType => typeof(EdgeController);
+
+    public EdgeData() { }
+
+    public EdgeData(int _firstNodeID, int _secondNodeID)
+    {
+        firstNodeID = _firstNodeID;
+        secondNodeID = _secondNodeID;
+    }
 }
