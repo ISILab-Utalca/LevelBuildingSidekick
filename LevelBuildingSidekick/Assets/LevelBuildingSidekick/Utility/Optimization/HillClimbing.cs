e7bf487e0f1c639c1de2737a6cf41921e5e5d861
pokelocos <nare.rare.fire@gmail.com>
2022-08-05 11:32:30 -0400

    Update HillClimbing.cs

--- LevelBuildingSidekick/Assets/LevelBuildingSidekick/Utility/Optimization/HillClimbing.cs
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Utility/Optimization/HillClimbing.cs
@@ -82,6 +82,57 @@ namespace Utility
             }
             return best;
         }
+
+        public static T Run<T, U>(T root, U heuristic, System.Func<bool> endCondition, System.Func<T, List<T>> GetNeighbors, System.Func<T, U, float> Evaluate)
+        {
+            var r = new System.Random();
+            score = 0;
+            nonSignificantEpochs = 0;
+            prevScore = score;
+            T best = root;
+            while (endCondition?.Invoke() == false)
+            {
+                prevScore = score;
+                score = Evaluate(best, heuristic);  
+                List<T> candidates = GetNeighbors(best);
+                List<T> betters = new List<T>();
+                Debug.Log("Before: " + candidates.Count);
+                float higherScore = score;
+                for (int i = 0; i < candidates.Count; i++)
+                {
+                    float newScore = Evaluate(candidates[i], heuristic);
+                    if (newScore > higherScore)
+                    {
+                        betters.Clear();
+                        betters.Add(candidates[i]);
+                    }
+                    else if (newScore == higherScore)
+                    {
+                        betters.Add(candidates[i]);
+                    }
+                    higherScore = higherScore < newScore ? newScore : higherScore;
+                    //Debug.Log("New Score: " + newScore);
+
+                }
+                if (higherScore <= score)
+                {
+                    nonSignificantEpochs++;
+                }
+                else
+                {
+                    nonSignificantEpochs = 0;
+                }
+                if (betters.Count == 0)
+                {
+                    return best;
+                }
+                Debug.Log("After: " + betters.Count);
+                best = betters[r.Next(0, betters.Count - 1)];
+                Debug.Log("Score: " + score);
+            }
+            return best;
+        }
+
     }
 }
 
