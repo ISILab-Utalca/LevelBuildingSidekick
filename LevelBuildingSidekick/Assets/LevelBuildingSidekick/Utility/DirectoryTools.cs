5d800261eb648d7fa2325b36ee049df0043e9e3b
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-08-08 09:46:42 -0400

    Create Utility.DirectoryTools

---
+++ LevelBuildingSidekick/Assets/LevelBuildingSidekick/Utility/DirectoryTools.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Utility
+{
+    public static class DirectoryTools
+    {
+        public static T SearchObjectByName<T>(string name)
+        {
+            var guids = AssetDatabase.FindAssets(name);
+            object obj = null;
+            foreach (var guid in guids)
+            {
+                obj = AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guid), typeof(object));
+                if(obj != null && obj is T)
+                {
+                    break;
+                }
+            }
+
+            return (T)obj;
+        }
+    }
+}
+
