90de569a0faeddddb4409a646540f9f78072ff4e
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-08-11 09:40:35 -0400

    GraphWindow

---
+++ LevelBuildingSidekick/Assets/Assets/GraphWindow.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+namespace LevelBuildingSidekick.Graph
+{
+    public class GraphWindow : EditorWindow
+    {
+        LBSGraphView graphView;
+
+        [MenuItem("Level Building Sidekick/Testing")]
+        public static void ShowExample()
+        {
+            GraphWindow wnd = GetWindow<GraphWindow>();
+            wnd.titleContent = new GUIContent("GraphWindow");
+        }
+
+        public void CreateGUI()
+        {
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+
+            // VisualElements objects can contain other VisualElement following a tree hierarchy.
+
+            // Import UXML
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Assets/GraphWindow.uxml");
+            visualTree.CloneTree(root);
+
+            // A stylesheet can be added to a VisualElement.
+            // The style will be applied to the VisualElement and all of its children.
+            var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Assets/GraphWindow.uss");
+            root.styleSheets.Add(styleSheet);
+
+            graphView = root.Q<LBSGraphView>();
+        }
+
+        private void OnSelectionChange()
+        {
+            graphView.PopulateView();
+        }
+    }
+}
