5037ac749cf7b8ad166609f337b4a98ee035b6fd
pokelocos <nare.rare.fire@gmail.com>
2022-08-24 17:21:26 -0400

    Update WindowsPreset.cs

--- LevelBuildingSidekick/Assets/LBS Plugin/Core/WindowPresets/WindowsPreset.cs
+++ LevelBuildingSidekick/Assets/LBS Plugin/Core/WindowPresets/WindowsPreset.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="New WindowsPreset", menuName ="LBS.../Presets.../WindowsPreset", order =0)]
 public class WindowsPreset : ScriptableObject
 {
+    [SerializeField]
+    private Texture2D thumnail;
+
     [SerializeField] 
     private List<string> windows = new List<string>();
 
+    public Texture2D Thumnail => thumnail;
     public List<string> Windows => new List<string>(windows); // no se puede pedir la lista entera para editarla solo para leerla
 
     public void AddWindow(string idWindow)
