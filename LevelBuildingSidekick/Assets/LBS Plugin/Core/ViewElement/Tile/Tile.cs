a1476d967c0f357a25c92a4fb6f69a132cdd8264
pokelocos <nare.rare.fire@gmail.com>
2022-08-24 17:08:39 -0400

    Update Tile

--- LevelBuildingSidekick/Assets/LBS Plugin/Core/ViewElement/Tile/Tile.cs
+++ LevelBuildingSidekick/Assets/LBS Plugin/Core/ViewElement/Tile/Tile.cs
@@ -1,19 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Tile : VisualElement
+namespace LBS.ElementView
 {
-    public new class UxmlFactory : UxmlFactory<Tile, VisualElement.UxmlTraits> { }
-
-    public Tile()
+    public class Tile : GraphElement
     {
-        var visualTree = Utility.DirectoryTools.SearchAssetByName<VisualTreeAsset>("Tile");
-        visualTree.CloneTree(this);
+        public new class UxmlFactory : UxmlFactory<Tile, VisualElement.UxmlTraits> { }
 
-        var styleSheet = Utility.DirectoryTools.SearchAssetByName<StyleSheet>("TileW");
-        this.styleSheets.Add(styleSheet);
+        public Tile()
+        {
+            var visualTree = Utility.DirectoryTools.SearchAssetByName<VisualTreeAsset>("Tile");
+            visualTree.CloneTree(this);
+
+            var styleSheet = Utility.DirectoryTools.SearchAssetByName<StyleSheet>("TileW");
+            this.styleSheets.Add(styleSheet);
+
+            this.SetPadding(0);
+            this.SetBorderWidth(0);
+            
+        }
+
+        private void SetPadding(int value)
+        {
+            this.style.paddingBottom = this.style.paddingLeft = this.style.paddingRight = this.style.paddingTop = value;
+        }
+
+        private void SetBorderWidth(int value)
+        {
+            this.style.borderBottomWidth = this.style.borderRightWidth = this.style.borderLeftWidth = this.style.borderTopWidth = value;
+        }
+
+        public void SetSize(int size)
+        {
+            SetSize(size, size);
+        }
+
+        public void SetSize(int x, int y)
+        {
+            this.style.width = this.style.maxWidth = this.style.minWidth = x;
+            this.style.height = this.style.maxHeight = this.style.minHeight = y;
+
+        }
     }
-
 }
