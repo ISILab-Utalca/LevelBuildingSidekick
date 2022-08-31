a12f1986ef2a19a8176f2e8f5df1abb6ff49fba4
pokelocos <nare.rare.fire@gmail.com>
2022-08-31 11:18:00 -0400

    Create stamp view

---
+++ LevelBuildingSidekick/Assets/ISILab/LBS Plugin/Level Representation/StampView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Utility;
+
+public class StampView : GraphElement // esto se puede simplificar y combiniar con nodeView
+{
+    private static readonly float size = 64;
+    private Texture2D texture;
+
+    private StampData data;
+
+    public StampView(StampData data)
+    {
+        this.data = data;
+        SetPosition(new Rect(data.Position, Vector2.one * size));
+
+        var stampPrest = DirectoryTools.GetScriptable<StampPresset>(data.Label);
+        this.style.backgroundImage = stampPrest.Icon;
+        this.style.minHeight = this.style.minWidth = this.style.maxHeight = this.style.maxWidth = size;
+
+        capabilities |= Capabilities.Selectable | Capabilities.Movable | Capabilities.Deletable | Capabilities.Ascendable | Capabilities.Copiable | Capabilities.Snappable | Capabilities.Groupable;
+        usageHints = UsageHints.DynamicTransform;
+    }
+
+    public override void SetPosition(Rect newPos)
+    {
+        base.SetPosition(newPos);
+        data.Position = new Vector2Int((int)newPos.x, (int)newPos.y);
+    }
+}
