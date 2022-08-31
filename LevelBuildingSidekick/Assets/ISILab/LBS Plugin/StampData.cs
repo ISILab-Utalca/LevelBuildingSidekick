db09b11ca15921d63c666cc3320e9bcb0b394bb7
pokelocos <nare.rare.fire@gmail.com>
2022-08-31 11:20:32 -0400

    Create stamp data

---
+++ LevelBuildingSidekick/Assets/ISILab/LBS Plugin/StampData.cs
@@ -0,0 +1,48 @@
+﻿using LevelBuildingSidekick;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using UnityEngine;
+
+
+[SerializeField]
+public class StampData : Data //cuestinable el nombre stamp pero no swe me ocurio otro (!!!)
+{
+    [HideInInspector, JsonRequired]
+    private int x, y;
+    [SerializeField, JsonRequired]
+    private string label = ""; // "ID" or "name"
+
+    /// <summary>
+    /// Empty constructor, necessary for serialization with json.
+    /// </summary>
+    public StampData() { }
+
+    public StampData(string label, Vector2 position)
+    {
+        this.label = label;
+        x = (int)position.x;
+        y = (int)position.y;
+    }
+
+    public override Type ControllerType => throw new NotImplementedException(); // no se usa en nada (!!!)
+
+    [JsonIgnore]
+    public string Label
+    {
+        get => label;
+        set => label = value;
+    }
+
+    [JsonIgnore]
+    public Vector2Int Position
+    {
+        get => new Vector2Int(x, y);
+
+        set
+        {
+            x = value.x;
+            y = value.y;
+        }
+    }
+}
