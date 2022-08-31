f04648c0b6cf163e44afba337a89de9db56fd7bc
pokelocos <nare.rare.fire@gmail.com>
2022-08-31 11:27:51 -0400

    add stampgroupdata

---
+++ LevelBuildingSidekick/Assets/ISILab/LBS Plugin/LBSStampGroupData.cs
@@ -0,0 +1,66 @@
+﻿using LevelBuildingSidekick;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LBSStampGroupData : LBSRepesentationData
+{
+    [SerializeField, JsonRequired, SerializeReference]
+    private List<StampData> stamps = new List<StampData>();
+
+    public override Type ControllerType => throw new NotImplementedException();
+
+    public StampData GetStamp(int index)
+    {
+        return stamps[index];
+    }
+
+    public StampData GetStamp(string label)
+    {
+        return stamps.Find(s => s.Label == label);
+    }
+
+    public void RemoveStamp(string label)
+    {
+        var r = stamps.Find(s => s.Label == label);
+        if (r != null)
+            RemoveNode(r);
+    }
+
+    public void RemoveNode(StampData node)
+    {
+        stamps.Remove(node);
+    }
+
+    /// <summary>
+    /// Returns a list with the stamps saved, this list is a
+    /// copy so you can not use this method to add or remove stamps.
+    /// </summary>
+    /// <returns></returns>
+    public List<StampData> GetStamps()
+    {
+        return new List<StampData>(stamps);
+    }
+
+    public void AddStamp(StampData stamp)
+    {
+        stamps.Add(stamp);
+    }
+
+    public override void Clear()
+    {
+        stamps.Clear();
+    }
+
+    public override void Print()
+    {
+        var msg = "";
+        msg += "<b>Stamp group. (step 2)</b>" + "\n";
+        msg += "stamp amount: " + this.stamps.Count + "\n";
+        msg += "------------";
+        stamps.ForEach(s => msg += s.Label + ": " + s.Position + "\n");
+        Debug.Log(msg);
+    }
+}
