e723dba7243733f3776891bc68114afd5f208dc1
pokelocos <nare.rare.fire@gmail.com>
2022-08-31 11:25:44 -0400

    add lbsviewcontroller

---
+++ LevelBuildingSidekick/Assets/ISILab/LBS Plugin/LBSViewController.cs
@@ -0,0 +1,35 @@
+﻿using LevelBuildingSidekick;
+using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public abstract class LBSViewController : Controller
+{
+    public List<ContextAction> contextActions = new List<ContextAction>();
+
+    protected LBSViewController(Data data) : base(data) { }
+
+    public override void LoadData()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        //throw new System.NotImplementedException();
+    }
+}
+
+public struct ContextAction
+{
+    public Action<DropdownMenuAction,LBSBaseView, ContextualMenuPopulateEvent> action;
+    public string name;
+
+    public ContextAction(string name, Action<DropdownMenuAction, LBSBaseView, ContextualMenuPopulateEvent> action)
+    {
+        this.action = action;
+        this.name = name;
+    }
+}
