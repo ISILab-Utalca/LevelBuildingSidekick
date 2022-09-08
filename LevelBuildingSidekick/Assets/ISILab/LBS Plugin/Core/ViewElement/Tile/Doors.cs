88b442a87ef7c92a712ec292348e1028c3d264f9
Irith14 <44303941+Irith14@users.noreply.github.com>
2022-09-08 19:00:10 -0300

    Door

---
+++ LevelBuildingSidekick/Assets/ISILab/LBS Plugin/Core/ViewElement/Tile/Doors.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Doors : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<Doors, VisualElement.UxmlTraits> { }
+
+    public VisualElement Left;
+    public VisualElement Right;
+    public VisualElement Top;
+    public VisualElement Botton;
+
+    public Doors()
+    {
+        var visualTree = Utility.DirectoryTools.SearchAssetByName<VisualTreeAsset>("Door");
+        visualTree.CloneTree(this);
+
+        Left = this.Q<VisualElement>(name: "Left");
+        Right = this.Q<VisualElement>(name: "Right");
+        Top = this.Q<VisualElement>(name: "Top");
+        Botton = this.Q<VisualElement>(name: "Botton");
+
+    }
+
+
+}
