374cb5ef49f38687c395b4c199a7502419f0ec0e
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-10-25 16:59:55 -0300

    Update Texture2DExtension.cs

--- LevelBuildingSidekick/Assets/ISILab/Commons/Utility/Texture2DExtension.cs
+++ LevelBuildingSidekick/Assets/ISILab/Commons/Utility/Texture2DExtension.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 namespace Utility
 {
@@ -9,12 +11,40 @@ namespace Utility
         public static void InsertTexture(this Texture2D origin, Texture2D other, int x, int y)
         {
             origin.SetPixels(x, y, other.width, other.height, other.GetPixels());
+            origin.Apply();
         }
 
         public static void InsertTextureInRect(this Texture2D origin, Texture2D other, int x, int y, int width, int height)
         {
             var pixels = Resizer.Resize2DArray(other.GetPixels(), other.width, other.height, width, height);
             origin.SetPixels(x, y, width, height, pixels);
+            origin.Apply();
+        }
+
+        public static void MirrorY(this Texture2D origin)
+        {
+            int height = origin.height;
+            Color[] pixels = new Color[origin.width*origin.height];
+            Array.Copy(origin.GetPixels(),pixels, pixels.Length);
+
+            for(int i = 0; i < height; i++)
+            {
+                origin.SetPixels(0, i, origin.width, 1, pixels.Skip((height - 1 - i) * origin.width).Take(origin.width).ToArray());
+            }
+            origin.Apply();
+        }
+
+        public static void MirrorY(this Texture2D origin, int stride)
+        {
+            int height = origin.height/stride;
+            Color[] pixels = new Color[origin.width * origin.height];
+            Array.Copy(origin.GetPixels(), pixels, pixels.Length);
+
+            for (int i = 0; i < height; i++)
+            {
+                origin.SetPixels(0, i, origin.width, stride, pixels.Skip((height - 1 - i) * origin.width).Take(origin.width*stride).ToArray());
+            }
+            origin.Apply();
         }
     }
 }
