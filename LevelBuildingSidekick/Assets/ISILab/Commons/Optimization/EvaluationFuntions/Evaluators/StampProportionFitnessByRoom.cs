9c8b6403d71b5066969009172ffa72f489f6ac5c
Gabriel Sepúlveda <gsepulveda17@alumnos.utalca.cl>
2022-10-28 12:54:05 -0300

    Proportion Evaluatoins by Room

---
+++ LevelBuildingSidekick/Assets/ISILab/Commons/Optimization/EvaluationFuntions/Evaluators/StampProportionFitnessByRoom.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Commons.Optimization.Evaluator;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using System.Linq;
+using LBS.Representation.TileMap;
+
+public class StampProportionFitnessByRoom : IRangedEvaluator
+{
+    public StampPresset stamp1;
+    public StampPresset stamp2;
+
+    float min = 0;
+    float max = 1;
+    public float MaxValue => max;
+
+    public float MinValue => min;
+
+    public VisualElement CIGUI()
+    {
+        var content = new VisualElement();
+
+        var v2 = new Vector2Field("Fitness threshold");
+        v2.value = new Vector2(this.MinValue, this.MaxValue);
+        v2.RegisterValueChangedCallback(e => {
+            min = e.newValue.x;
+            max = e.newValue.y;
+        });
+
+        ObjectField of1 = new ObjectField("Stamp 1: ");
+        of1.objectType = typeof(StampPresset);
+        of1.value = stamp1;
+        of1.RegisterCallback<ChangeEvent<StampPresset>>((e) => stamp1 = e.newValue);
+
+
+        ObjectField of2 = new ObjectField("Stamp 2: ");
+        of2.objectType = typeof(StampPresset);
+        of2.value = stamp2;
+        of2.RegisterCallback<ChangeEvent<StampPresset>>((e) => stamp2 = e.newValue);
+
+        content.Add(v2);
+        content.Add(of1);
+        content.Add(of2);
+
+        return content;
+    }
+
+    public float Evaluate(IEvaluable evaluable)
+    {
+        if(!(evaluable is StampTileMapChromosome))
+        {
+            return MinValue;
+        }
+
+        var stmc = evaluable as StampTileMapChromosome;
+        var data = stmc.GetGenes<int>();
+
+        var p1count = stmc.stamps.Select(s => s.Label == stamp1.Label).Count();
+        var p2count = stmc.stamps.Select(s => s.Label == stamp2.Label).Count();
+
+        if (p1count == 0 || p2count == 0)
+        {
+            if(p1count == 0 && p2count == 0)
+            {
+                return MaxValue;
+            }
+
+            return MinValue;
+        }
+
+        var rooms = (StampTileMapChromosome.TileMap.GetData() as LBSTileMapData).GetRooms();
+
+        var fitness = 0;
+
+        foreach (var r in rooms)
+        {
+            int counterP1 = 0;
+            int counterP2 = 0;
+            foreach (var t in r.Tiles)
+            {
+                int val = stmc.GetGene<int>(stmc.ToIndex(t.GetPosition()));
+                if (val == -1) continue;
+                if (stamp1.Label == stmc.stamps[val].Label)
+                {
+                    counterP1++;
+                }
+                if (stamp2.Label == stmc.stamps[val].Label)
+                {
+                    counterP2++;
+                }
+            }
+            var c = counterP1 / counterP2;
+            c = c > 1 ? 1 / c : c;
+            fitness += c;
+        }
+
+        return fitness/rooms.Count;
+    }
+
+    public string GetName()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ LevelBuildingSidekick/Assets/ISILab/Commons/Optimization/EvaluationFuntions/Evaluators/TagProportionByRoom.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Commons.Optimization.Evaluator;
+using UnityEngine.UIElements;
+using System.Linq;
+using LBS;
+using LBS.Representation.TileMap;
+
+public class TagProportionByRoom : IRangedEvaluator
+{
+
+    public string tag1;
+    public string tag2;
+
+    float min = 0;
+    float max = 1;
+    public float MaxValue => max;
+
+    public float MinValue => min;
+
+
+    public VisualElement CIGUI()
+    {
+        var content = new VisualElement();
+        var tags = Utility.DirectoryTools.GetScriptable<LBSTags>("Brush tags").Alls;
+
+        var v2 = new Vector2Field("Fitness threshold");
+        v2.value = new Vector2(this.MinValue, this.MaxValue);
+        v2.RegisterValueChangedCallback(e => {
+            min = e.newValue.x;
+            max = e.newValue.y;
+        });
+
+        var index1 = tags.FindIndex(t => t == tag1);
+        var index2 = tags.FindIndex(t => t == tag2);
+
+        DropdownField tagDD1 = new DropdownField("Tag 1: ");
+        tagDD1.choices = tags;
+        tagDD1.index = index1;
+        tagDD1.RegisterCallback<ChangeEvent<string>>(e => tag1 = e.newValue);
+
+        DropdownField tagDD2 = new DropdownField("Tag 2: ");
+        tagDD2.choices = tags;
+        tagDD2.index = index2;
+        tagDD2.RegisterCallback<ChangeEvent<string>>(e => tag2 = e.newValue);
+
+        content.Add(v2);
+        content.Add(tagDD1);
+        content.Add(tagDD2);
+
+        return content;
+    }
+
+    public float Evaluate(IEvaluable evaluable)
+    {
+        if(!(evaluable is StampTileMapChromosome))
+        {
+            return MinValue;
+        }
+
+        var stmc = evaluable as StampTileMapChromosome;
+        var data = stmc.GetGenes<int>();
+
+        var pressets = Utility.DirectoryTools.GetScriptables<StampPresset>();
+
+        var pressetsG1 = pressets.Where(p => p.Tags.Contains(tag1)).Select(p => p.Label);
+        var pressetsG2 = pressets.Where(p => p.Tags.Contains(tag2)).Select(p => p.Label);
+
+        var indexG1 = new List<int>();
+        var indexG2 = new List<int>();
+
+        if (pressetsG1.Count() == 0 || pressetsG2.Count() == 0)
+        {
+            if (pressetsG1.Count() == 0 && pressetsG2.Count() == 0)
+            {
+                return MaxValue;
+            }
+
+            return MinValue;
+        }
+
+        foreach (var pr in pressetsG1)
+        {
+            indexG1.Add(stmc.stamps.FindIndex(s => s.Label == pr));
+        }
+
+        foreach (var pr in pressetsG2)
+        {
+            indexG2.Add(stmc.stamps.FindIndex(s => s.Label == pr));
+        }
+
+        var rooms = (StampTileMapChromosome.TileMap.GetData() as LBSTileMapData).GetRooms();
+
+        var fitness = 0;
+
+        foreach (var r in rooms)
+        {
+
+            int counterG1 = 0;
+            int counterG2 = 0;
+
+            foreach (var t in r.Tiles)
+            {
+                int val = stmc.GetGene<int>(stmc.ToIndex(t.GetPosition()));
+                if (val == -1) continue;
+                if (indexG1.Contains(val))
+                {
+                    counterG1++;
+                }
+                else if(indexG2.Contains(stmc.GetGene<int>(stmc.ToIndex(t.GetPosition()))))
+                {
+                    counterG2++;
+                }
+            }
+            var c = counterG1 / counterG2;
+            c = c > 1 ? 1 / c : c;
+            fitness += c;
+        }
+
+        return fitness/rooms.Count;
+    }
+
+    public string GetName()
+    {
+        return "Tag Proportion";
+    }
+}
