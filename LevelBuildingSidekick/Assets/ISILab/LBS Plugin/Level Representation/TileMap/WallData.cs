e172d75146a8cb30d08b5102f81da3c077ec5454
pokelocos <nare.rare.fire@gmail.com>
2022-11-21 12:17:48 -0300

    Update WallData.cs

--- LevelBuildingSidekick/Assets/ISILab/LBS Plugin/Level Representation/TileMap/WallData.cs
+++ LevelBuildingSidekick/Assets/ISILab/LBS Plugin/Level Representation/TileMap/WallData.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace LBS.Representation.TileMap
@@ -9,28 +10,27 @@ namespace LBS.Representation.TileMap
     [Serializable]
     internal class WallData : ICloneable // esto no corresponde a una muralla como tal sino que a los tiles de borde de una habitacion
     {
-        // info
+        // Fields
         internal string ownerID;
-        internal Vector2Int firstCorner;
-        internal Vector2Int secondCorner;
-
-        // Metainfo
         internal Vector2Int dir; // direccion a la que mira con respecto a su habitacion
         internal List<Vector2Int> allTiles;
 
-        public WallData(Vector2Int corner1, Vector2Int corner2, string ownerID, Vector2Int dir, List<Vector2Int> allTiles = null)
-        {
-            this.firstCorner = corner1;
-            this.secondCorner = corner2;
-            this.ownerID = ownerID;
+        // Properties
+        public Vector2Int First => allTiles.First();
+        public Vector2Int Last => allTiles.Last();
 
+        // Constructors
+        public WallData(string ownerID, Vector2Int dir, List<Vector2Int> allTiles = null)
+        {
+            this.ownerID = ownerID;
             this.dir = dir;
             this.allTiles = allTiles;
         }
 
-        public object Clone()
+        // Methods
+        public object Clone() // (!!) implementar
         {
-            return null;
+            throw new System.NotImplementedException();
         }
     }
 }
