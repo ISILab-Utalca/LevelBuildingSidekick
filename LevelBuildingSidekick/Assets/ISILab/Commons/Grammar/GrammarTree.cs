5f6d0e8ceb307cccea502bd4a7ce4114d68eed8c
Irith14 <43820153+Prion6@users.noreply.github.com>
2023-01-03 09:09:01 -0300

    Update GrammarTree.cs

--- LevelBuildingSidekick/Assets/ISILab/Commons/Grammar/GrammarTree.cs
+++ LevelBuildingSidekick/Assets/ISILab/Commons/Grammar/GrammarTree.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Speech.Recognition.SrgsGrammar;
+using System;
 
 public class GrammarTree
 {
     public Dictionary<string, TerminalNode> Terminals = new Dictionary<string, TerminalNode>();
     public Dictionary<string, NonTerminalNode> NonTerminals = new Dictionary<string, NonTerminalNode>();
     public Dictionary<string, ProductionNode> Productions = new Dictionary<string, ProductionNode>();
+    public Dictionary<string, GrammarNode> Actions = new Dictionary<string, GrammarNode>();
 
-    public GrammarNode Root { get; set; }
+    private GrammarNode root;
+    public GrammarNode Root { 
+        get => root;
+        set 
+        {
+            root = value;
+            UpdateRoot();
+        } 
+    }
 
     public GrammarTree()
     {
@@ -18,6 +28,77 @@ public class GrammarTree
     public GrammarTree(GrammarNode root)
     {
         Root = root;
+    }
+
+    public void UpdateRoot()
+    {
+        while (true)
+        {
+            //Debug.Log(root.GetType().Name);
+            if (root is TerminalNode)
+                break;
+            if (root is ProductionNode)
+            {
+                var node = (root as ProductionNode);
+                if (node.Nodes.Count > 1)
+                {
+                    //Debug.Log(Root.ID);
+                    break;
+                }
+                root = node.Nodes[0];
+                //Debug.Log("Out");
+                continue;
+            }
+            if (root is NonTerminalNode)
+            {
+                var node = (root as NonTerminalNode);
+                if (node.Nodes.Count > 1)
+                {
+                    //Debug.Log(root.ID);
+                    break;
+                }
+                root = node.Nodes[0];
+                //Debug.Log("Out");
+                continue;
+            }
+        }
+
+        Actions.Clear();
+
+        if (root is NonTerminalNode)
+        {
+            var nodes = (root as NonTerminalNode).Nodes;
+            foreach (var node in nodes)
+            {
+                Actions.Add(node.ID, node);
+            }
+        }
+        else if (root is ProductionNode)
+        {
+            var nodes = (root as ProductionNode).Nodes;
+            foreach (var node in nodes)
+            {
+                Actions.Add(node.ID, node);
+            }
+        }
+    }
+
+    internal GrammarNode GetGrammarElement(string grammarKey)
+    {
+        if (Terminals.ContainsKey(grammarKey))
+        {
+            return Terminals[grammarKey];
+        }
+        else if (NonTerminals.ContainsKey(grammarKey))
+        {
+            return NonTerminals[grammarKey];
+        }
+        else if(Productions.ContainsKey(grammarKey))
+        {
+            return Productions[grammarKey];
+        }
+        return null;
+
     }
 
     public string TextSentence()
