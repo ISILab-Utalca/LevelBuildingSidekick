a925e446a434499c394ff37f7e1abf6104e8b5a0
Irith14 <43820153+Prion6@users.noreply.github.com>
2023-01-03 09:10:45 -0300

    Update QuestGraphNode.cs

--- LevelBuildingSidekick/Assets/ISILab/LBS Plugin/Level Representation/Quest/QuestGraphNode.cs
+++ LevelBuildingSidekick/Assets/ISILab/LBS Plugin/Level Representation/Quest/QuestGraphNode.cs
@@ -6,11 +6,68 @@ using UnityEngine;
 public class QuestGraphNode : LBSNodeData
 {
     string grammarKey;
+    public string GrammarKey => grammarKey;
 
-    public QuestGraphNode(string label, Vector2 position) : base(label, position) 
+    private int expandTo;
+
+    QuestGraphNode parent;
+    public QuestGraphNode Parent
+    {
+        get => parent;
+        set => parent = value;
+    }
+
+    List<QuestGraphNode> children;
+
+    public List<QuestGraphNode> Children
+    {
+        get
+        {
+            if (children == null)
+                children = new List<QuestGraphNode>();
+            return children;
+        }
+        set => children = value;
+    }
+
+    public int ExpandTo
+    {
+        get => expandTo;
+        set => expandTo = value;
+    }
+
+    public QuestGraphNode(string label, Vector2 position, QuestGraphNode parent = null) : base(label, position)
     {
         this.grammarKey = label;
-        Debug.Log(Label);
         Label = label;
+        expandTo = -1;
+        children = new List<QuestGraphNode>();
+        Parent = parent;
+    }
+
+    public void Expand(int option, GrammarTree grammar)
+    {
+
+        /*if (option == expandTo)
+            return;*/
+
+        expandTo = option;
+        var element = grammar.GetGrammarElement(grammarKey);
+
+
+
+        if (element == null)
+        {
+            return;
+        }
+
+        var nodes = element.GetExpansion(option);
+
+        Children.Clear();
+
+        for(int i = 0; i < nodes.Count; i++)
+        {
+            Children.Add(new QuestGraphNode(nodes[i].ID, new Vector2(i*Width*2.5f,0), this));
+        }
     }
 }
