8b64a3f13f71ad39d8194e8dddff6a71624194c8
Irith14 <43820153+Prion6@users.noreply.github.com>
2023-01-10 12:14:47 -0300

    Update LBSModule.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/LBSModule.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/LBSModule.cs
@@ -1,17 +1,57 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace LBS.Components
 {
+    [System.Serializable]
     public abstract class LBSModule
     {
+        //FIELDS
+        [SerializeField, JsonRequired, SerializeReference]
+        string id;
+
+        [SerializeField, JsonRequired, SerializeReference]
         bool visible;
+
+        [SerializeField, JsonRequired, SerializeReference]
+        bool changed;
+
+        //PROPERTIES
+        [JsonIgnore]
         public bool IsVisible
         {
             get => visible;
             set => visible = value;
         }
+
+        [JsonIgnore]
+        public bool HasChanged
+        {
+            get => changed;
+            set => changed = value;
+        }
+
+        [JsonIgnore]
+        public string ID
+        {
+            get => id;
+            set => id = value;
+        }
+
+        //METHODS
+
+        /// <summary>
+        /// prints by console basic information of 
+        /// the representation.
+        /// </summary>
+        public abstract void Print();
+
+        /// <summary>
+        /// Cleans all the information saved in.
+        /// </summary>
+        public abstract void Clear();
     }
 }
 
