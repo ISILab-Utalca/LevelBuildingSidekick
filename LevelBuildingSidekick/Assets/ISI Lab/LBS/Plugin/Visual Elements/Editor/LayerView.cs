509b008bfb7be988d39bc835fda0c382e9b9168c
pokelocos <nare.rare.fire@gmail.com>
2023-01-11 16:59:53 -0300

    Add layersPanel and view

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LayerView.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LayerView.cs
@@ -7,9 +7,19 @@ public class LayerView : VisualElement
 {
     public new class UxmlFactory : UxmlFactory<LayerView, VisualElement.UxmlTraits> { }
 
+    private Label layerName;
+
     public LayerView()
     {
         var visualTree = Utility.DirectoryTools.SearchAssetByName<VisualTreeAsset>("LayerView"); // Editor
         visualTree.CloneTree(this);
+
+        // LayerName
+        layerName= this.Q<Label>("LayerName");
+    }
+
+    public void SetName(string name)
+    {
+        layerName.text = name;
     }
 }
--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LayersPanel.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LayersPanel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,5 +10,33 @@ public class LayersPanel : VisualElement
     {
         var visualTree = Utility.DirectoryTools.SearchAssetByName<VisualTreeAsset>("LayersPanel"); // Editor
         visualTree.CloneTree(this);
+
+        // LayerList
+        var list = this.Q<ListView>("LayerList");
+        Func<VisualElement> makeItem = () => new Label();
+        Action<VisualElement, int> bindItem = (l, i) => (l as Label).text = i.ToString();
+        //Func<VisualElement> makeItem = () => new LayerView();
+        //Action<VisualElement, int> bindItem = (l, n) => (l as LayerView).SetName(n.ToString());
+        list.fixedItemHeight = 16;
+        list.makeItem += makeItem;
+        list.bindItem += bindItem;
+        //list.unbindItem += ;
+        //list.itemsAdded += ;
+        //list.itemsRemoved += ;
+        //list.itemsSource += ;
+        list.onItemsChosen += OnItemChosen;
+        list.onSelectionChange += OnSelectionChange; 
+
+        
+    }
+
+    public void OnSelectionChange(IEnumerable<object> objs)
+    {
+        Debug.Log("OSC");
+    }
+
+    public void OnItemChosen(IEnumerable<object> objs)
+    {
+        Debug.Log("OIC");
     }
 }
