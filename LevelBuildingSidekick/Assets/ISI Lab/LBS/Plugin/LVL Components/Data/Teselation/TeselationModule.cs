8bfb0420dcb515aa852c21377f95378fa5dc96f8
Irith14 <43820153+Prion6@users.noreply.github.com>
2023-01-12 09:20:58 -0300

    Teselation

---
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Teselation/TeselationModule.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LBS.Components.Teselation
+{
+    public abstract class TeselationModule : LBSModule
+    {
+        Vector2 CellSize;
+
+        public Vector2 SnapPosition(Vector2 position)
+        {
+            Vector2 snappedPosition = new Vector2(
+                Mathf.Round(position.x / CellSize.x) * CellSize.x,
+                Mathf.Round(position.y / CellSize.y) * CellSize.y
+            );
+            return snappedPosition;
+        }
+
+        public Vector2Int ToMatrixPosition(Vector2 position)
+        {
+            Vector2 snappedPosition = SnapPosition(position);
+            Vector2Int matrixPosition = new Vector2Int(
+                Mathf.RoundToInt(snappedPosition.x / CellSize.x),
+                Mathf.RoundToInt(snappedPosition.y / CellSize.y)
+            );
+            return matrixPosition;
+        }
+
+        public Vector2 ToWorldPosition(Vector2Int matrixPosition)
+        {
+            Vector2 worldPosition = new Vector2(
+                matrixPosition.x * CellSize.x,
+                matrixPosition.y * CellSize.y
+            );
+            return worldPosition;
+        }
+    }
+}
+
