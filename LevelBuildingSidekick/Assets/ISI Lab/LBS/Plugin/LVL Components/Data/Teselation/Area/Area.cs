296204d88f84f470f0637387890aad97036e00d5
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-01-16 18:04:37 -0300

    Update Area.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Teselation/Area/Area.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Teselation/Area/Area.cs
@@ -1,13 +1,49 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Area
 {
-    List<Polygon> polygons;
+    [SerializeField, JsonRequired, SerializeReference]
+    Polygon perimeter;
+
+    [JsonIgnore]
+    public int PerimeterVertexCount => perimeter.Points.Count;
+
+    public bool AddVertex(Vector2 vertex)
+    {
+        perimeter.Points.Add(vertex);
+        return true;
+    }
+
+    public Vector2 GetVertex(int index)
+    {
+        return perimeter.Points[index];
+    }
+
+    public bool Remove(Vector2 vertex)
+    {
+        return perimeter.Points.Remove(vertex);
+    }
+
+    public Vector2 RemoveAt(int index)
+    {
+        var v = perimeter.Points[index];
+        perimeter.Points.Remove(v);
+        return v;
+    }
 }
 
+[System.Serializable]
 struct Polygon
 {
-    List<Vector2> Points;
+    [SerializeField, JsonRequired, SerializeReference]
+    public List<Vector2> Points;
+
+    public Polygon(List<Vector2> points)
+    {
+        Points = points;
+    }
 }
