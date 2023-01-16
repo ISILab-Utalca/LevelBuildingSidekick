5ec9fe079f67ef5054f20dffabdc5ef2122991b7
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-01-16 15:34:57 -0300

    Node with RoomData reference

---
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Specifics/Graph/RoomNode.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LBS.Components.Graph;
+
+namespace LBS.Components.Specifics
+{
+    public class RoomNode : LBSNode
+    {
+        RoomData room;
+
+        public RoomData Room => room;
+
+        public RoomNode(): base()
+        {
+            room = null;
+        }
+
+        public RoomNode(RoomData room) : base()
+        {
+            this.room = room;
+        }
+    }
+}
+
+
