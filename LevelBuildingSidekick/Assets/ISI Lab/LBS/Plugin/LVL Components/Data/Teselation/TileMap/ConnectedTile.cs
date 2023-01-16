bf9997fc6b46924c46f83acb86f277e14998edd7
pokelocos <nare.rare.fire@gmail.com>
2023-01-16 09:13:21 -0300

    Update ConnectedTile.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Teselation/TileMap/ConnectedTile.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Teselation/TileMap/ConnectedTile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -45,7 +46,7 @@ namespace LBS.Components.TileMap
 
         public bool RemoveConnection(string connection)
         {
-
+            throw new NotImplementedException();
         }
     }
 }
