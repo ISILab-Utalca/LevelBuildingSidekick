801548010fa032f7d77f79f13b6c65d02251ecf1
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-01-17 17:02:30 -0300

    Update Area.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Teselation/Area/Area.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Teselation/Area/Area.cs
@@ -2,16 +2,60 @@ using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [System.Serializable]
 public class Area
 {
+    //FIELDS
     [SerializeField, JsonRequired, SerializeReference]
     Polygon perimeter;
 
+    [SerializeField, JsonRequired, SerializeReference]
+    string id;
+
+    //PROPERTIES
     [JsonIgnore]
     public int PerimeterVertexCount => perimeter.Points.Count;
 
+    [JsonIgnore]
+    public string ID => id;
+
+    public Vector2 Centroid
+    {
+        get
+        {
+            var x = perimeter.Points.Sum(p => p.x) / perimeter.Points.Count;
+            var y = perimeter.Points.Sum(p => p.y) / perimeter.Points.Count;
+            return new Vector2(x,y);
+        }
+        set
+        {
+            var x = perimeter.Points.Min(p => p.x);
+            var y = perimeter.Points.Min(p => p.y);
+
+            var delta = new Vector2(value.x - x, value.y - y);
+
+            for(int i = 0; i < PerimeterVertexCount; i++)
+            {
+                perimeter.Points[i] += delta;
+            }
+        }
+    }
+
+    //COSNTRUCTOR
+    public Area()
+    {
+        perimeter = new Polygon(new List<Vector2>());
+    }
+
+    public Area(string id, List<Vector2> vertexes)
+    {
+        this.id = id;
+        perimeter = new Polygon(vertexes);
+    }
+
+    //METHODS
     public bool AddVertex(Vector2 vertex)
     {
         perimeter.Points.Add(vertex);
@@ -33,6 +77,16 @@ public class Area
         var v = perimeter.Points[index];
         perimeter.Points.Remove(v);
         return v;
+    }
+
+    internal void Clear()
+    {
+        perimeter.Points.Clear();
+    }
+
+    public bool Collides(Area other)//Implementar
+    {
+        return false;
     }
 }
 
