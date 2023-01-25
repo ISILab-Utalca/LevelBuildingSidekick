f21d7056f962ac305be714caf586599e1efd3d5b
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-01-25 11:43:45 -0300

    Update TiledArea.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Specifics/TileMap/TiledArea.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/Specifics/TileMap/TiledArea.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 
 namespace LBS.Components.TileMap
 {
+    [System.Serializable]
     public class TiledArea<T> : TileMapModule<T> where T : LBSTile
     {
         #region FIELDS
