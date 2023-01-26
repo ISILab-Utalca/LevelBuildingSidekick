b105b11d61dd702f0da0fccbb116fba446c3a8b9
Ignacio Negrón <inegron18@alumnos.utalca.cl>
2023-01-26 17:57:28 -0300

    Update AddDoor.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Manipulators/AddDoor.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Manipulators/AddDoor.cs
@@ -1,57 +1,39 @@
 using LBS.Components;
-using LBS.Components.Graph;
+using LBS.Components.Teselation;
 using LBS.Components.TileMap;
 using LBS.ElementView;
-using LBS.Representation.TileMap;
-using LBS.Representation;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.UIElements;
+using LBS.Representation;
+using LBS.Representation.TileMap;
+using UnityEditor.PackageManager.UI;
 
-public class AddDoor<T> : LBSManipulator where T : LBSTile
+public class AddDoor<T, U> : ManipulateTiledArea<T, U> where T : TiledArea<U> where U : LBSTile
 {
-    private TileMapModule<T> module;
-    private LBSTileMapController controller;
+    private List<Vector2Int> tiles;
     private TileData first;
 
-    public override void Init(ref MainView view, ref LBSLevelData level, ref LBSLayer layer, ref LBSModule module)
-    {
-        this.module = layer.GetModule<TileMapModule<T>>();
-    }
-
-    protected override void RegisterCallbacksOnTarget()
-    {
-        target.RegisterCallback<MouseDownEvent>(OnMouseDown);
-        target.RegisterCallback<MouseMoveEvent>(OnMouseMove);
-        target.RegisterCallback<MouseUpEvent>(OnMouseUp);
-    }
-
-    protected override void UnregisterCallbacksFromTarget()
-    {
-        target.UnregisterCallback<MouseDownEvent>(OnMouseDown);
-        target.UnregisterCallback<MouseMoveEvent>(OnMouseMove);
-        target.UnregisterCallback<MouseUpEvent>(OnMouseUp);
-    }
-
-    private void OnMouseDown(MouseDownEvent e)
+    protected override void OnMouseDown(MouseDownEvent e)
     {
         OnManipulationStart?.Invoke();
+        tiles = new List<Vector2Int>();
+
         var tile = e.target as LBSTileView;
         if (tile == null)
             return;
 
         first = tile.Data;
-
     }
 
-    private void OnMouseMove(MouseMoveEvent e)
+    protected override void OnMouseMove(MouseMoveEvent e)
     {
-        //Debug.Log("Move drag");
+       
     }
 
-    private void OnMouseUp(MouseUpEvent e)
+    protected override void OnMouseUp(MouseUpEvent e)
     {
         if (first == null)
             return;
@@ -62,15 +44,10 @@ public class AddDoor<T> : LBSManipulator where T : LBSTile
 
         var second = tile.Data;
 
-        /*
-        var schema = controller.GetData() as LBSSchemaData;
-        var r1 = schema.GetRoom(first.Position);
-        var r2 = schema.GetRoom(second.Position);
-        if (r1.Equals(r2))
+        //var r1 = module.GetRoom(first.Position.);
+        //var r2 = module.GetRoom(second.Position);
+        //if (r1.Equals(r2))
             return;
-        */ 
-        
-
 
         var dx = Mathf.Abs(first.GetPosition().x - second.GetPosition().x);
         var dy = Mathf.Abs(first.GetPosition().y - second.GetPosition().y);
@@ -82,7 +59,7 @@ public class AddDoor<T> : LBSManipulator where T : LBSTile
             second.GetPosition()
             );
 
-        controller.AddDoor(door);
         OnManipulationEnd?.Invoke();
     }
+
 }
