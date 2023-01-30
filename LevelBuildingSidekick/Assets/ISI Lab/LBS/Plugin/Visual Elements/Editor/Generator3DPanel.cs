853ad80a26fd8c1c5362b3a77e439ba1f35be08c
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-01-30 17:27:45 -0300

    Update Generator3DPanel.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/Generator3DPanel.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/Generator3DPanel.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using LBS.Generator;
+using LBS.Components;
 
 public class Generator3DPanel : VisualElement
 {
+    Generator generator;
+    ClassDropDown dropDown;
+
+    public Generator Generator
+    {
+        get => generator;
+        set => generator = value;
+    }
+
     public new class UxmlFactory : UxmlFactory<Generator3DPanel, VisualElement.UxmlTraits> { }
 
     public Generator3DPanel() { }
 
-    public Generator3DPanel(LBSLevelData levelData)
+    public Generator3DPanel(LBSLevelData data)
     {
         var visualTree = Utility.DirectoryTools.SearchAssetByName<VisualTreeAsset>("Generator3DPanel"); // Editor
         visualTree.CloneTree(this);
+
+        dropDown = this.Q<ClassDropDown>();
+        dropDown.Label = "Gennerator";
+        dropDown.Type = typeof(Generator);
+
+
     }
 }
