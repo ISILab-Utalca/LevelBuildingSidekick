6367e9dd563971ea798dbbef4025c76b909a7d10
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-01-30 04:36:23 -0300

    Update Generator.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Generators3D/Generator.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Generators3D/Generator.cs
@@ -2,14 +2,15 @@ using LBS;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LBS.Components;
 
 namespace LBS.Generator
 {
     public abstract class Generator
     {
-        public abstract GameObject Generate();
+        public abstract GameObject Generate(LBSLayer layer);
 
-        public abstract void Init(LBSLevelData levelData);
+        public abstract void Init(LBSLayer layer);
 
         //public abstract void Init(Layer layer); // (!!) implementar
     }
