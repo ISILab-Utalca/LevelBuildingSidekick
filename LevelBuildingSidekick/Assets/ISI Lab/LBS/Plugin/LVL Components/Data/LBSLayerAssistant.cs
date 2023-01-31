8146f0e33aee53c7cfa70e14101b3f4a1335b16d
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-01-31 17:12:12 -0300

    Layer Metadat en otro clase
    necesita cambio de nombre

---
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/LBSLayerAssistant.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LBS.Generator;
+using Newtonsoft.Json;
+
+public class LBSLayerAssistant : ScriptableObject
+{
+    [SerializeField, SerializeReference]
+    Generator3D generator;
+
+    public Generator3D Generator
+    {
+        get => generator;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
