3a7b57f60ca37638e33d19bb31c698642dcd9e61
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-02-01 15:27:45 -0300

    Update LBSLayerAssistant.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/LBSLayerAssistant.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Data/LBSLayerAssistant.cs
@@ -3,28 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using LBS.Generator;
 using Newtonsoft.Json;
+using LBS.AI;
 
 public class LBSLayerAssistant : ScriptableObject
 {
     [SerializeField, SerializeReference]
     Generator3D generator;
 
-
+    [SerializeField, SerializeReference]
+    List<LBSAIAgent> aiAgents;
 
     public Generator3D Generator
     {
         get => generator;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
