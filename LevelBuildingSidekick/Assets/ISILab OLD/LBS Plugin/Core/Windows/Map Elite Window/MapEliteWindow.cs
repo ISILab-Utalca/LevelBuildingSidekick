66d2a13d8aab233fef97c6d1999a2b7b3ab459be
pokelocos <nare.rare.fire@gmail.com>
2023-02-02 02:31:34 -0300

    Primera purga

--- LevelBuildingSidekick/Assets/ISILab OLD/LBS Plugin/Core/Windows/Map Elite Window/MapEliteWindow.cs
+++ LevelBuildingSidekick/Assets/ISILab OLD/LBS Plugin/Core/Windows/Map Elite Window/MapEliteWindow.cs
@@ -3,7 +3,7 @@ using LBS.VisualElements;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.UIElements;
+
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Linq;
