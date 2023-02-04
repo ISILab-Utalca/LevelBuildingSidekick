dae8a530125056f8082afda7a32e68a35dc00866
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-02-04 20:02:16 -0300

    Update LBSTagsBundle.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Tags/LBSTagsBundle.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Tags/LBSTagsBundle.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(fileName = "New tags bundle", menuName = "ISILab/Tags Bundle")]
 public class LBSTagsBundle : ScriptableObject
 {
-    public List<LBSTag> tags = new List<LBSTag>();
+    [SerializeField]
+    [ScriptableToString(typeof(LBSTag))]
+    List<string> tags = new List<string>();
+
+    public List<LBSTag> Tags => tags.Select(t => Utility.DirectoryTools.GetScriptable<LBSTag>(t)).ToList();
 
     public void AddTag(LBSTag tag)
     {
-        tags.Add(tag);
+        tags.Add(tag.value);
     }
 }
