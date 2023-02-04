2ca0fd71782630f53b2a4fc125656d013cd3cfa1
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-02-04 20:02:04 -0300

    Update PopulationBundle.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Bundles/PopulationBundle.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Bundles/PopulationBundle.cs
@@ -2,13 +2,12 @@ using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu(fileName = "New bundle", menuName = "ISILab/PopulationBundle")]
 [System.Serializable]
-public class PopulationBundle : Bundle
+public class PopulationBundle : SimpleBundle
 {
-    [SerializeReference, JsonRequired, SerializeField]
-    private List<GameObject> objects = new List<GameObject>();
     [JsonRequired, SerializeField]
     private Texture2D icon;
     [JsonRequired, SerializeField]
@@ -16,41 +15,4 @@ public class PopulationBundle : Bundle
     [JsonIgnore]
     public string Label => label;
 
-    public override void Add(List<Bundle> data)
-    {
-        foreach (Bundle bundle in data)
-        {
-            tags.AddRange(bundle.GetTags());
-            objects.AddRange(bundle.GetObjects());
-        }
-    }
-
-    public override GameObject GetObject(int index)
-    {
-        return objects[index];
-    }
-
-    public override List<GameObject> GetObjects(List<string> tags = null)
-    {
-        return objects;
-    }
-
-    public override LBSTag GetTag(int index)
-    {
-        return tags[index];
-    }
-
-    public override List<LBSTag> GetTags()
-    {
-        return tags;
-    }
-
-    public override void Remove(List<Bundle> data)
-    {
-        foreach (Bundle bundle in data)
-        {
-            tags.RemoveAll(bundle.GetTags().Contains);
-            objects.RemoveAll(bundle.GetObjects().Contains);
-        }
-    }
 }
