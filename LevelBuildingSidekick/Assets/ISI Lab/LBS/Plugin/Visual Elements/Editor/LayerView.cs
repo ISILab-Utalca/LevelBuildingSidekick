7173bb0123707d74f8dd6eaa7785d8c696df86d4
pokelocos <nare.rare.fire@gmail.com>
2023-03-15 12:17:59 -0300

    Update LayerView.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LayerView.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LayerView.cs
@@ -1,3 +1,5 @@
+using LBS.Components;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +9,21 @@ public class LayerView : VisualElement
 {
     public new class UxmlFactory : UxmlFactory<LayerView, VisualElement.UxmlTraits> { }
 
-    private Label layerName;
+    private LBSLayer target;
+
+    private TextField layerName;
     private VisualElement layerIcon;
+    private VisualElement iconsModules;
+    private Button showButton;
+    private Button hideButton;
+
+    private Action onVisibilityChange;
+
+    public event Action OnVisibilityChange
+    {
+        add => onVisibilityChange += value;
+        remove => onVisibilityChange -= value;
+    }
 
     public LayerView()
     {
@@ -16,20 +31,68 @@ public class LayerView : VisualElement
         visualTree.CloneTree(this);
 
         // LayerName
-        layerName= this.Q<Label>("LayerName");
+        this.layerName = this.Q<TextField>("LayerName");
+        this.layerName.RegisterCallback<ChangeEvent<string>>(e => {
+            this.target.Name = e.newValue;
+        });
 
         // LayerIcon
-        layerIcon = this.Q<VisualElement>("LayerIcon");
+        this.layerIcon = this.Q<VisualElement>("LayerIcon");
+
+        // IconsModules
+        this.iconsModules = this.Q<VisualElement>("IconsModules");
+
+        // Show/Hide button
+        this.showButton = this.Q<Button>("ShowButton");
+        this.showButton.clicked += () => { ShowLayer(true);};
+        this.hideButton = this.Q<Button>("HideButton");
+        this.hideButton.clicked += () => ShowLayer(false);
     }
 
-    public void SetName(string name)
+    private void SetName(string name)
     {
-        layerName.text = name;
+        layerName.value = name;
     }
 
-    public void SetIcon(string name)
+    private void SetIcon(string name)
     {
         var texture = Resources.Load<Texture2D>(name);
         layerIcon.style.backgroundImage = texture;
+    }
+
+    public void SetInfo(LBSLayer layer)
+    {
+        this.target = layer;
+
+        SetIcon(layer.iconPath);
+        SetName(layer.Name);
+
+        layer.OnModuleChange += (layer) => {
+            ShowModulesIcons();
+        };
+
+        ShowLayer(layer.IsVisible);
+    }
+
+    private void ShowModulesIcons()
+    {
+        iconsModules.Clear();
+
+        foreach (var module in target.Modules)
+        {
+            var icon = new VisualElement();
+            icon.style.height = icon.style.width = 16;
+            // Set "icon" background from "moduleAttribute" pathicon (implementar)(!!!)
+            iconsModules.Add(icon);
+        }
+    }
+
+    private void ShowLayer(bool value)
+    {
+        showButton.style.display = (!value) ? DisplayStyle.Flex : DisplayStyle.None;
+        hideButton.style.display = (value) ? DisplayStyle.Flex : DisplayStyle.None;
+
+        target.IsVisible = value;
+        onVisibilityChange?.Invoke();
     }
 }
