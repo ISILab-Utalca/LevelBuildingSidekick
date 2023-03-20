e35796a60b03b4563e8af3161c9d9ad47041c704
pokelocos <nare.rare.fire@gmail.com>
2023-03-20 12:56:39 -0300

    Update WFCBundle.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Bundles/WFCBundle.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Bundles/WFCBundle.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+// Parche (!!!), esta clase en vez de tener un objeto "LBSDirection" deberia añadirlo dentro de su lista de characteristic
+// pero aun no se puede hacer hasta que se sobreescriba la parte visual y se pueda añadir uno especifico
 [CreateAssetMenu(fileName = "New exterior bundle", menuName = "ISILab/Bundles/Exterior")]
-public class WFCBundle : ScriptableObject // Parche
+public class WFCBundle : Bundle  
 {
     [Range(0, 1)]
     public float weight = 1;
 
-    public LBSDirection directions = new LBSDirection();
+    public LBSDirection directions = new LBSDirection(); // Foward, Rigth, Bottom, Left
 
-    public List<weightedTile> Pref;
+    public List<weightedTile> Pref = new List<weightedTile>();
+
+    public override void Add(List<Bundle> data)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override List<LBSCharacteristic> GetCharacteristics()
+    {
+        return new List<LBSCharacteristic>(this.characteristics);
+    }
 
     public string[] GetConnection(int rotation = 0)
     {
@@ -23,6 +35,26 @@ public class WFCBundle : ScriptableObject // Parche
         }
 
         return conections.ToArray();
+    }
+
+    public override GameObject GetObject(int index)
+    {
+        return Pref[index].gameObject;
+    }
+
+    public override List<GameObject> GetObjects(List<string> tags = null)
+    {
+        return Pref.Select(p => p.gameObject).ToList();
+    }
+
+    public override LBSCharacteristic GetTag(int index)
+    {
+        return this.characteristics[index];
+    }
+
+    public override void Remove(List<Bundle> data)
+    {
+        throw new System.NotImplementedException();
     }
 
     private string[] Rotate(string[] c)
