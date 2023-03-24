05427bee732bd52cf283a2e4587609b9b9f6543f
pokelocos <nare.rare.fire@gmail.com>
2023-03-24 12:31:13 -0300

    Update Feedback.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/Feedback/Feedback.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/Feedback/Feedback.cs
@@ -20,10 +20,10 @@ public abstract class Feedback : GraphElement
     private State currentState; 
     
     protected Color currentColor = Color.white;
-    protected Vector2Int startPosition;
-    protected Vector2Int endPosition;
+    protected Vector2Int startPosition = new Vector2Int();
+    protected Vector2Int endPosition = new Vector2Int();
 
-    protected bool fixToTeselation = false;
+    public bool fixToTeselation = false;
     #endregion
 
     #region PROPERTIES
@@ -58,28 +58,51 @@ public abstract class Feedback : GraphElement
         this.SetPosition(new Rect(startPosition, new Vector2(10, 10)));
         this.generateVisualContent += OnGenerateVisualContent;
     }
+
+    public Feedback()
+    {
+        this.ActualizePositions(startPosition, endPosition);
+        this.SetPosition(new Rect(startPosition, new Vector2(10, 10)));
+        this.generateVisualContent += OnGenerateVisualContent;
+    }
+
     #endregion
 
     #region METHODS
     protected abstract void OnGenerateVisualContent(MeshGenerationContext mgc);
 
-    protected void ActualizePositions(Vector2Int p1, Vector2Int p2)
+    public void ActualizePositions(Vector2Int p1, Vector2Int p2)
     {
         startPosition = new Vector2Int(Mathf.Min(p1.x, p2.x), Mathf.Min(p1.y, p2.y));
         endPosition = new Vector2Int(Mathf.Max(p1.x, p2.x), Mathf.Max(p1.y, p2.y));
-
+        
         if (fixToTeselation)
         {
-            startPosition = new Vector2Int(
-                startPosition.x / teselationSize.x,
-                startPosition.y / teselationSize.y) 
-                * teselationSize;
-            endPosition = new Vector2Int(
-                endPosition.x / teselationSize.x,
-                endPosition.y / teselationSize.y)
-                * teselationSize;
+            var spX = (startPosition.x >= 0) ?
+                (startPosition.x / teselationSize.x) :
+                (startPosition.x / teselationSize.x) - 1;
+            var spY = (startPosition.y >= 0) ?
+                (startPosition.y / teselationSize.y) :
+                (startPosition.y / teselationSize.y) - 1;
+            startPosition = new Vector2Int(spX, spY);
+
+            var epX = (endPosition.x >= 0) ?
+                (endPosition.x / teselationSize.x) :
+                (endPosition.x / teselationSize.x) - 1;
+            var epY = (endPosition.y >= 0) ?
+                (endPosition.y / teselationSize.y) :
+                (endPosition.y / teselationSize.y) - 1;
+            endPosition = new Vector2Int(epX, epY);
+
+            endPosition.x += 1;
+            endPosition.y += 1;
+
+            startPosition *= teselationSize;
+            endPosition *= teselationSize;
         }
 
+
+        this.MarkDirtyRepaint();
     }
     #endregion
 
@@ -93,16 +116,37 @@ public abstract class Feedback : GraphElement
 
 public class DotedAreaFeedback : Feedback
 {
-    public DotedAreaFeedback(Vector2Int p1, Vector2Int p2) : base(p1, p2)
-    {
+    public DotedAreaFeedback(Vector2Int p1, Vector2Int p2) : base(p1, p2) { }
 
-    }
+    public DotedAreaFeedback() : base() { }
 
     protected override void OnGenerateVisualContent(MeshGenerationContext mgc)
     {
         var painter = mgc.painter2D;
-        var view = this.GetFirstAncestorOfType<MainView>();
+        var fillColor = currentColor * new Color(1, 1, 1, 0.1f);
 
-        painter.DrawBox(startPosition, endPosition, currentColor, 4);
+        var points = new List<Vector2>()
+        {
+            new Vector2(startPosition.x, startPosition.y),
+            new Vector2(startPosition.x, endPosition.y),
+            new Vector2(endPosition.x, endPosition.y),
+            new Vector2(endPosition.x, startPosition.y),
+        };
+        painter.DrawPoligon(points, fillColor, currentColor, 4, true);
+    }
+}
+
+public class ConectedLine : Feedback
+{
+    public ConectedLine(Vector2Int p1, Vector2Int p2) : base(p1, p2) { }
+    public ConectedLine() : base() { }
+
+    protected override void OnGenerateVisualContent(MeshGenerationContext mgc)
+    {
+        var painter = mgc.painter2D;
+        var line = new List<Vector2>() { startPosition, endPosition };
+        painter.DrawPoligon(line, new Color(0,0,0,0), currentColor, 4, false);
+        painter.DrawCircle(startPosition, 16, currentColor);
+        painter.DrawCircle(endPosition, 16, currentColor);
     }
 }
