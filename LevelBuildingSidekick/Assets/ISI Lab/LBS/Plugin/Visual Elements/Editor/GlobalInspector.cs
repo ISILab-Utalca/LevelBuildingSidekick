8c26da575b9784ab2dfd8ca56a2d53d38ee0eaf8
pokelocos <nare.rare.fire@gmail.com>
2023-04-10 16:49:10 -0400

    add global inspector

---
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/GlobalInspector.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GlobalInspector : VisualElement
+{
+
+    public GlobalInspector()
+    {
+
+    }
+
+}
