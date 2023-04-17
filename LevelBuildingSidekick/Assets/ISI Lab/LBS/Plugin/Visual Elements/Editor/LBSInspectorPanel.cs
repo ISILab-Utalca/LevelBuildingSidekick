42e643739fd38046f6170442bbf3ef2b5814dbf3
pokelocos <nare.rare.fire@gmail.com>
2023-04-17 17:48:22 -0400

    update inspectorPanel

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LBSInspectorPanel.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LBSInspectorPanel.cs
@@ -14,8 +14,12 @@ public class LBSInspectorPanel : VisualElement
 
     private List<LBSInspector> inspectors = new List<LBSInspector>();
 
-    private ButtonGroup subTabs;
+    private ButtonGroup subTab;
     private ButtonGroup mainTab;
+
+    private Dictionary<string, Dictionary<string, VisualElement>> VEs = new Dictionary<string, Dictionary<string, VisualElement>>();
+
+    #region CONSTRUCTORS
 
     public LBSInspectorPanel() 
     {
@@ -23,60 +27,182 @@ public class LBSInspectorPanel : VisualElement
         visualTree.CloneTree(this);
 
         // SubTabs
-        subTabs = this.Q<ButtonGroup>("SubTabs");
-        subTabs.Init();
+        subTab = this.Q<ButtonGroup>("SubTabs");
+        subTab.Init();
 
         // MainButtonGroup
         mainTab = this.Q<ButtonGroup>("MainTabs");
         mainTab.Init();
-        SetSubTab();
 
         // Content
         content = this.Q<VisualElement>("InspectorContent");
+
+        InitTabs();
+        SetTabs("Global");
+
+        //SetSubTab();
     }
 
+    /*
     public LBSInspectorPanel(ref MainView mainView, VisualElement content)
     {
         this.view = mainView;
         this.content = content;
     }
+    */
 
-    private void SetSubTab()
+    #endregion
+
+    private void InitTabs()
     {
-        var tabs = mainTab.Children().Select(st => st as IGrupable).ToList();
-
-        tabs[0].OnFocusEvent += () =>
-        {
-            var btn = tabs[0] as GrupalbeButton;
-            Debug.Log(btn.text);
-        };
-
-        tabs[1].OnFocusEvent += () =>
-        {
-            var btn = tabs[1] as GrupalbeButton;
-            Debug.Log(btn.text);
-        };
-
-        tabs[2].OnFocusEvent += () =>
-        {
-            var btn = tabs[2] as GrupalbeButton;
-            Debug.Log(btn.text);
-        };
+        var gt = new VisualElement();
+        gt.style.backgroundColor = Color.red;
+        gt.style.flexGrow = 1;
+        AddTab("Global", "Tags", gt);
+        var gb = new VisualElement();
+        gb.style.backgroundColor = Color.blue;
+        gb.style.flexGrow = 1;
+        AddTab("Global", "Bundles", gb);
 
 
-        subTabs.Choices = "Tags,Bundles,Assistants";
+        var l1 = new VisualElement();
+        l1.style.backgroundColor = Color.magenta;
+        l1.style.flexGrow = 1;
+        AddTab("Local", "Current data", l1);
+        var l2 = new VisualElement();
+        l2.style.backgroundColor = Color.gray;
+        l2.style.flexGrow = 1;
+        AddTab("Local", "Behaviours", l2);
+        var l3 = new VisualElement();
+        l3.style.backgroundColor = Color.yellow;
+        l3.style.flexGrow = 1;
+        AddTab("Local", "Assistants", l3);
+
+
+        var aset = new VisualElement();
+        aset.style.backgroundColor = Color.green;
+        aset.style.flexGrow = 1;
+        AddTab("Advanced", "Settings", aset);
+        var aset2 = new VisualElement();
+        aset2.style.backgroundColor = Color.cyan;
+        aset2.style.flexGrow = 1;
+        AddTab("Advanced", "Settings", aset2);
     }
 
-    public void AddInspector(LBSInspector inspector)
+    private void AddTab(string mainTabName, string subTabName, VisualElement element)
+    {
+        if(VEs.ContainsKey(mainTabName))
+        {
+            VEs.TryGetValue(mainTabName, out var dic);
+            if (dic.ContainsKey(subTabName))
+            {
+                VEs.TryGetValue(mainTabName, out var visualElement);
+            }
+            else
+            {
+                dic.Add(subTabName, element);
+            }
+        }
+        else
+        {
+            var newDic = new Dictionary<string, VisualElement>();
+            newDic.Add(subTabName, element);
+            VEs.Add(mainTabName, newDic);
+        }
+
+        var mainTabs = mainTab.Children().ToList();
+        var subTabs = subTab.Children().ToList();
+
+        mainTabs.Select(s => s.name).Any();
+
+    }
+
+    private void SetTabs(string name)
+    {
+        mainTab.Choices = string.Join(",", VEs.Select(pv => pv.Key));
+
+        var mTabs = mainTab.Children().Select(st => st as IGrupable).ToList();
+        foreach (var btn in mTabs)
+        {
+            btn.OnFocusEvent += () =>
+            {
+                var btn2 = btn as GrupalbeButton;
+                Debug.Log(btn2.text);
+                SetSubTabs(btn2.text);
+
+                this.ClearContent();
+                VEs.TryGetValue(btn2.text, out var ve);
+                var xx = ve.Keys.ToList();
+                ve.TryGetValue(xx[0], out var inspct);
+                this.SetContent(inspct);
+            };
+        }
+
+        SetSubTabs(name);
+
+        this.ClearContent();
+        VEs.TryGetValue(name, out var ve);
+        var xx = ve.Keys.ToList();
+        ve.TryGetValue(xx[0], out var inspct);
+        this.SetContent(inspct);
+    }
+
+    private void SetSubTabs(string name)
+    {
+        VEs.TryGetValue(name, out var ve);
+        subTab.Choices = string.Join(",", ve.Select(pv => pv.Key));
+
+        var sTabs = subTab.Children().Select(st => st as IGrupable).ToList();
+        for (int i = 0; i < sTabs.Count(); i++)
+        {
+            var btn = sTabs[i];
+
+            btn.OnFocusEvent += () =>
+            {
+                var btn2 = btn as GrupalbeButton;
+                this.ClearContent();
+                ve.TryGetValue(btn2.text,out var inspct);
+                this.SetContent(inspct);
+            };
+        }
+    }
+
+    private void ClearContent()
+    {
+        content.Clear();
+    }
+
+    private void SetContent(VisualElement inspector)
+    {
+        if (inspector == null)
+            return;
+
+        inspector.style.height = 30; // parche para ver lo que esta pasando (!!)
+        content.Add(inspector);
+    }
+
+    public void AddInspector(LBSInspector inspector) // relacionado con toolkit (!!!)
     {
         inspectors.Add(inspector);
         content.Add(inspector);
     }
 
-    public void RemoveInspector(LBSInspector inspector)
+    public void RemoveInspector(LBSInspector inspector) // relacionado con toolkit (!!!)
     {
         inspectors.Remove(inspector);
         if(content.Contains(inspector))
             content.Remove(inspector);
     }
+
+    private void ShowGlobalTagInspector()
+    {
+
+
+    }
+
+    private void ShowGlobalBundlesInspector()
+    {
+
+    }
+
 }
