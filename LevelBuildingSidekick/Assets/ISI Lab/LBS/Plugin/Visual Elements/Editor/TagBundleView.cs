bd022fa2e5c3da3d8fc00b23fac3556d8e349076
pokelocos <nare.rare.fire@gmail.com>
2023-04-24 08:53:15 -0400

    update tag bundle view

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/TagBundleView.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/TagBundleView.cs
@@ -1,9 +1,12 @@
+using LBS.Settings;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEngine.UI.InputField;
 
 public class TagBundleView : VisualElement
 {
@@ -13,6 +16,10 @@ public class TagBundleView : VisualElement
     private Toggle toggle;
     private TextField groupNameField;
     private ListView list;
+    private Button addBtn;
+    private Button removeBtn;
+
+    private LBSIdentifier selected;
 
     public TagBundleView()
     {
@@ -22,67 +29,100 @@ public class TagBundleView : VisualElement
         this.box = this.Q<VisualElement>("Box");
         this.toggle = this.Q<Toggle>();
         this.groupNameField = this.Q<TextField>();
-        groupNameField.RegisterCallback<ChangeEvent<string>>(e => OnTextChange(e.newValue));
 
+        groupNameField.RegisterCallback<BlurEvent>(e => OnTextChange(groupNameField.value));
+        //groupNameField.RegisterCallback<ChangeEvent<string>>(e => OnTextChange(e.newValue));
 
         list = this.Q<ListView>();
 
-        Func<VisualElement> makeItem = () =>
-        {
-            return new TagView();
-        };
-
         list.fixedItemHeight = 22;
-        list.makeItem += makeItem;
+        list.makeItem += MakeItem;
+        list.bindItem += BindItem;
         list.onItemsChosen += OnItemChosen;
         list.onSelectionChange += OnSelectionChange;
         list.style.flexGrow = 1.0f;
+
+        addBtn = this.Q<Button>("Add");
+        addBtn.clicked += CreateTag;
+        removeBtn = this.Q<Button>("Remove");
+        removeBtn.clicked += RemoveTag;
+    }
+
+    private VisualElement MakeItem()
+    {
+        return new TagView();
+    }
+
+    private void CreateTag()
+    {
+        var name = ISILab.Commons.Commons.CheckNameFormat(target.Tags.Select(b => b.name), "new tag");
+
+        var nTag = ScriptableObject.CreateInstance<LBSIdentifier>();
+        nTag.Init(name, new Color().RandomColor(), null);
+
+        var settings = LBSSettings.Instance;
+
+        AssetDatabase.CreateAsset(nTag, settings.tagFolderPath + "/" + name + ".asset");
+        AssetDatabase.SaveAssets();
+
+        Debug.Log(target.GetInstanceID());
+        if(!target.Tags.Contains(nTag))
+            target.AddTag(nTag); 
+
+        list.itemsSource = target.Tags;
+
+        list.Rebuild();
+        list.RefreshItems();
+    }
+
+    private void RemoveTag()
+    {
+        if (selected == null)
+            return;
+
+        target.Remove(selected);
+
+        var path = AssetDatabase.GetAssetPath(selected);
+        AssetDatabase.DeleteAsset(path);
+        AssetDatabase.SaveAssets();
+        
+        list.itemsSource = target.Tags;
+        list.Rebuild();
     }
 
     private void OnTextChange(string value)
     {
         target.name = value;
+        AssetDatabase.RenameAsset(AssetDatabase.GetAssetPath(target), target.name);
+        EditorUtility.SetDirty(target);
     }
 
 
     public void SetInfo(LBSIdentifierBundle tagBundle)
     {
         target = tagBundle;
+        list.itemsSource = target.Tags;
 
         groupNameField.value = target.name;
 
-        list.bindItem += (item, index) =>
-        {
-            var view = (item as TagView);
-        
-            if (index >= this.target.Tags.Count())
-                return;
-
-            var tag = this.target.GetTag(index);
-            view.SetInfo(tag);
-        };
-
-        list.itemsSource = this.target.Tags;
-
-    }
-
-    private void RemoveSelected()
-    {
-        if (target.Tags.Count <= 0)
-            return;
-
-        var index = list.selectedIndex;
-        if (index <= 0)
-            return;
-
-        var tag = target.GetTag(index);
-        target.RemoveAt(index);
         list.Rebuild();
     }
 
+    public void BindItem(VisualElement ve, int index)
+    {
+        var view = (ve as TagView);
+
+        if (index >= this.target.Tags.Count())
+            return;
+
+        var tag = this.target.GetTag(index);
+        view.SetInfo(tag);
+    }
+
+
     public void OnSelectionChange(IEnumerable<object> objs)
     {
-        var selected = objs.ToList()[0] as LBSIdentifier;
+        selected = objs.ToList()[0] as LBSIdentifier;
     }
 
     public void OnItemChosen(IEnumerable<object> objs)
