ce1075ba61264d65f970f75d0ea76f282d8878be
pokelocos <nare.rare.fire@gmail.com>
2023-04-24 15:49:09 -0400

    create general bundles panel

---
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/Globals Inspector/GeneralBundlesPanel.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class GeneralBundlesPanel : VisualElement
+{
+    #region FACTORY
+    public new class UxmlFactory : UxmlFactory<GeneralBundlesPanel, VisualElement.UxmlTraits> { }
+    #endregion
+
+    private ObjectField iconField;
+    private TextField textField;
+    private ColorField colorField;
+
+    public GeneralBundlesPanel()
+    {
+        var visualTree = Utility.DirectoryTools.SearchAssetByName<VisualTreeAsset>("GeneralBundlesPanel");
+        visualTree.CloneTree(this);
+
+        this.iconField = this.Q<ObjectField>("IconField");
+        this.textField = this.Q<TextField>("NameField");
+        this.colorField = this.Q<ColorField>("ColorField");
+    }
+}
