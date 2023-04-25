4205d9fd6a5a9d62d2d200148da740f3ae6654f5
pokelocos <nare.rare.fire@gmail.com>
2023-04-25 10:11:32 -0400

    Update Commons.cs

--- LevelBuildingSidekick/Assets/ISI Lab/Commons/Utility/Commons.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/Commons/Utility/Commons.cs
@@ -10,7 +10,7 @@ namespace ISILab.Commons
         public static int indexFormat = 0;
         public static readonly string[] numberFormat = new string[] { " #", "_#", " (#)" };
 
-        public static string CheckNameFormat(IEnumerable<string> context, string value, bool firstFormat = false, string prefix = "", string postfix = "")        {
+        public static string CheckNameFormat(IEnumerable<string> context, string value, bool firstFormat = false, string prefix = "", string postfix = "", string specificFormat = "")        {
             var name = "";
             var loop = true;
             var v = 0;
@@ -19,7 +19,12 @@ namespace ISILab.Commons
                 name = prefix + value + postfix;
 
                 if (firstFormat)
-                    name = prefix + value + postfix + numberFormat[indexFormat].Replace("#", v.ToString());
+                {
+                    if(specificFormat == "")
+                        name = prefix + value + postfix + numberFormat[indexFormat].Replace("#", v.ToString());
+                    else
+                        name = prefix + value + postfix + specificFormat.Replace("#", v.ToString());
+                }
 
                 firstFormat = true;
 
