e934865980d1fa5d3d2a867bc459cfa3e281128a
pokelocos <nare.rare.fire@gmail.com>
2023-05-22 13:18:23 -0400

    fix SchemaHC

--- LevelBuildingSidekick/Assets/ISI Lab/Commons/Extensions/CollectionsExtensions.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/Commons/Extensions/CollectionsExtensions.cs
@@ -10,6 +10,16 @@ public static class CollectionsExtensions
     {
         return index >= 0 && index < list.Count;
     }
+    public static T GetRandom<T>(this List<T> list)
+    {
+        if (list.Count <= 0)
+        {
+            Debug.Log("[ISI Lab]: Error to try get a random element in '" + list + "' because is empty.");
+            return default(T);
+        }
+
+        return list[UnityEngine.Random.Range(0, list.Count - 1)];
+    }
 
     #endregion
 
@@ -18,6 +28,17 @@ public static class CollectionsExtensions
     public static bool ContainsIndex<T>(this T[] array, int index)
     {
         return index >= 0 && index < array.Length;
+    }
+
+    public static T GetRandom<T>(this T[] array)
+    {
+        if (array.Length <= 0)
+        {
+            Debug.Log("[ISI Lab]: Error to try get a random element in '"+array+"' because is empty.");
+            return default(T);
+        }
+
+        return array[UnityEngine.Random.Range(0,array.Length - 1)];
     }
 
     #endregion
