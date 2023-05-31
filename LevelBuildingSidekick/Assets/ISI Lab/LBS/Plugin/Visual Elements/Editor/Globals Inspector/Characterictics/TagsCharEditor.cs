9917c7e11e0d5c2dc6185092f06dad8182b6410c
pokelocos <nare.rare.fire@gmail.com>
2023-05-31 11:00:08 -0400

    Update TagsCharEditor.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/Globals Inspector/Characterictics/TagsCharEditor.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/Globals Inspector/Characterictics/TagsCharEditor.cs
@@ -1,27 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 [LBSCustomEditor(typeof(LBSTagsCharacteristic))]
 public class TagsCharEditor : LBSCustomEditor
 {
-    public TextField idField;
+    public TextField labelField;
     public DropdownField dropdownField;
+
+    public LBSTagsCharacteristic target;
 
     public TagsCharEditor()
     {
-        idField = new TextField();
-        this.Add(idField);
+        var storage = LBSAssetsStorage.Instance;
+
+        labelField = new TextField();
+        this.Add(labelField);
+        labelField.RegisterCallback<BlurEvent>(e => {
+            target.Label = labelField.value;
+        });
 
         dropdownField = new DropdownField();
         this.Add(dropdownField);
+        var tags = storage.Get<LBSIdentifier>();
+        dropdownField.choices = tags.Select(t => t.Label).ToList();
+        dropdownField.RegisterCallback<ChangeEvent<string>>(e => {
+            target.Value = tags.Find(t => t.Label == e.newValue);
+        });
+
     }
 
     public override void SetInfo(object target)
     {
-        var t = target as LBSTagsCharacteristic;
+        this.target = target as LBSTagsCharacteristic;
 
-        idField.value = t.ToString();
+        labelField.value = this.target?.Label;
+        dropdownField.value = this.target?.Value?.Label;
     }
 }
