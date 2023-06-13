68d962f411328808d8245b1e3ed27a39b6f2ab7a
pokelocos <nare.rare.fire@gmail.com>
2023-06-13 09:36:02 -0400

    Update WFCBehaviour.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Behaviours/WFCBehaviour.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Behaviours/WFCBehaviour.cs
@@ -6,5 +6,8 @@ using UnityEngine;
 public class WFCBehaviour : LBSBehaviour
 {
     //private WaveFunctionCollapse<LBSTile> wfc = new WaveFunctionCollapse<LBSTile>();
-
+    public override object Clone()
+    {
+        throw new System.NotImplementedException();
+    }
 }
