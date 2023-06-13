13a0b1935541a22660f4834b1d666a4af6a5ea81
pokelocos <nare.rare.fire@gmail.com>
2023-06-13 09:34:29 -0400

    metadata attribute

---
+++ LevelBuildingSidekick/Assets/ISI Lab/Commons/Attributes/Metadata.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Metadata
+{
+    public string name;
+    public string description;
+    public Texture2D icon;
+}
---
+++ LevelBuildingSidekick/Assets/ISI Lab/Commons/Attributes/MetadataAttribute.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using static LBSAssistantAI;
+
+public class MetadataAttribute : Attribute
+{
+    public Metadata metadata;
+
+    public MetadataAttribute(string name,string texturePath, string descrition = "")
+    {
+        var texture = AssetDatabase.LoadAssetAtPath<Texture2D>(texturePath);
+        metadata = new Metadata()
+        {
+            name = name,
+            icon = texture,
+            description = descrition,
+        };
+    }
+}
