f46749906e427a5058c0ed4d8e3db3b350d6c9c0
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-06-13 22:53:40 -0400

    Update LBSMainWindow.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LBSMainWindow.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/LBSMainWindow.cs
@@ -178,12 +178,9 @@ public class LBSMainWindow : EditorWindow
 
         // AIPanel
         aiPanel = new AIPanel();
-        aiPanel.OnAIExecute += () =>
+        aiPanel.OnFinish += () =>
         {
             drawManager.RefreshView(ref _selectedLayer, levelData.Layers, _selectedMode);
-        };
-        aiPanel.OnEndExecute += () =>
-        {
             OnSelectedLayerChange2(_selectedLayer);
         };
 
