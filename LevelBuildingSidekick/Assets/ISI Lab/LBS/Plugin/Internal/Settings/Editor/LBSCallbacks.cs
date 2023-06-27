6d7925a919d697627f6c2c3f85e2412698a78ca2
pokelocos <nare.rare.fire@gmail.com>
2023-06-27 17:51:09 -0400

    Imagens and others

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Internal/Settings/Editor/LBSCallbacks.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Internal/Settings/Editor/LBSCallbacks.cs
@@ -2,8 +2,69 @@ using LBS;
 using LBS.Settings;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+
+[InitializeOnLoad]
+public class CheckPackages
+{
+    static ListRequest Request;
+
+    static CheckPackages()
+    {
+        CheckForPackages();
+    }
+
+    static void CheckForPackages()
+    {
+        Request = Client.List(offlineMode: true);
+        EditorApplication.update += progress;
+    }
+
+    static void progress()
+    {
+        string packageId = "com.unity.nuget.newtonsoft-json";
+
+        if (Request.IsCompleted)
+        {
+            if (Request.Status == StatusCode.Success)
+            {
+                if (!ContainsPackage(Request.Result, packageId))
+                {
+                    Debug.Log("Package '" + packageId + "' is missing");
+                    string installCommand = "com.unity.nuget.newtonsoft-json";
+                    //AssetDatabase.ImportPackage("Packages/com.unity.nuget.newtonsoft-json", true);
+                    Client.Add(installCommand);
+
+                }
+            }
+            else if (Request.Status >= StatusCode.Failure)
+            {
+                Debug.Log("Could not check for packages: " + Request.Error.message);
+            }
+
+            EditorApplication.update -= progress;
+        }
+    }
+
+    public static bool ContainsPackage(PackageCollection packages, string packageId)
+    {
+        foreach (var package in packages)
+        {
+            if (string.Compare(package.name, packageId) == 0)
+                return true;
+
+            foreach (var dependencyInfo in package.dependencies)
+                if (string.Compare(dependencyInfo.name, packageId) == 0)
+                    return true;
+        }
+
+        return false;
+    }
+}
 
 [InitializeOnLoad]
 public class LBSCallbacks
