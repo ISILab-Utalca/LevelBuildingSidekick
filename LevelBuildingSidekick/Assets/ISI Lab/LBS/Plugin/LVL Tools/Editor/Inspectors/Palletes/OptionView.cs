af89a2bcea43a410024e0e14cefe6b04823516e6
pokelocos <nare.rare.fire@gmail.com>
2023-08-07 18:03:09 -0400

    add optionview

---
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Inspectors/Palletes/OptionView.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Utility;
+
+namespace LBS.VisualElements
+{
+    public class OptionView : VisualElement
+    {
+        private Label label;
+        private Button button;
+
+        private object target;
+
+        public Action<object> OnSelect;
+        private Action<OptionView, object> OnSetView;
+
+        #region PROPERTIES
+        public object Target
+        {
+            get => target;
+            set
+            {
+                target = value;
+                OnSetView?.Invoke(this, target);
+            }
+        }
+
+        public string Label
+        {
+            set => label.text = value;
+        }
+
+        public Texture2D Icon
+        {
+            set => button.style.backgroundImage = value;
+        }
+
+        public Color Color
+        {
+            set => button.style.backgroundColor = value;
+        }
+        #endregion
+
+        public OptionView(object target, Action<object> onSelect, Action<OptionView, object> onSetView)
+        {
+            var visualTree = DirectoryTools.SearchAssetByName<VisualTreeAsset>("OptionView");
+            visualTree.CloneTree(this);
+
+            // Init View
+            this.label = this.Q<Label>();
+            this.button = this.Q<Button>();
+
+            // Init Fields
+            this.target = target;
+
+            // Init Events
+            this.OnSelect = onSelect;
+
+            this.OnSetView = onSetView;
+            OnSetView?.Invoke(this, target);
+        }
+    }
+}
