dff2fd0c402fbc9a10bdad0a4f691967a1604483
pokelocos <nare.rare.fire@gmail.com>
2023-08-07 18:05:11 -0400

    Update LBSLocalBehaviours.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/InspectorElements/LBSInspector/LBSLocalBehaviours.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/InspectorElements/LBSInspector/LBSLocalBehaviours.cs
@@ -1,4 +1,5 @@
 using LBS.Components;
+using LBS.VisualElements;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,11 +14,15 @@ public class LBSLocalBehaviours : LBSInspector
     public new class UxmlFactory : UxmlFactory<LBSLocalBehaviours, VisualElement.UxmlTraits> { }
     #endregion
 
+    private Color colorBH = new Color(135f / 255f, 215f / 255f, 246f / 255f);
+
     private VisualElement content;
     private VisualElement noContentPanel;
     private VisualElement contentBehaviour;
 
     private LBSLayer target;
+
+    private ToolKit toolkit;
 
     public LBSLocalBehaviours()
     {
@@ -27,6 +32,8 @@ public class LBSLocalBehaviours : LBSInspector
         content = this.Q<VisualElement>("Content");
         this.noContentPanel = this.Q<VisualElement>("NoContentPanel");
         this.contentBehaviour = this.Q<VisualElement>("ContentBehaviour");
+
+        toolkit = ToolKit.Instance;
     }
 
     public void SetInfo(LBSLayer target)
@@ -41,45 +48,37 @@ public class LBSLocalBehaviours : LBSInspector
             return;
         }
 
-        foreach (var b in target.Behaviours)
+        foreach (var behaviour in target.Behaviours)
         {
-            var type = b.GetType();
-            var ves = Utility.Reflection.GetClassesWith<LBSCustomEditorAttribute>().Where(t => t.Item2.Any(v => v.type == type));
+            var type = behaviour.GetType();
+            var ves = Utility.Reflection.GetClassesWith<LBSCustomEditorAttribute>()
+                .Where(t => t.Item2.Any(v => v.type == type)).ToList();
+
             if (ves.Count() == 0)
             {
                 Debug.LogWarning("[ISI Lab] No class marked as LBSCustomEditor found for type: " + type);
                 continue;
             }
 
-            var ve = Activator.CreateInstance(ves.First().Item1, new object[] { b });
+            var ovg = ves.First().Item1;
+            var ve = Activator.CreateInstance(ovg, new object[] { behaviour });
             if (!(ve is VisualElement))
             {
                 Debug.LogWarning("[ISI Lab] " + ve.GetType() + " is not a VisualElement ");
                 continue;
             }
 
-            contentContainer.Add(ve as VisualElement);
-        }
-        /*
-        foreach (var behaviour in target.Behaviours)
-        {
-            var so = Utility.Reflection.MakeGenericScriptable(behaviour);
-            var editor = Editor.CreateEditor(so);
-
-            var cont = new VisualElement();
-            cont.style.backgroundColor = new Color(0, 0, 0, 0.1f);
-            cont.SetBorder(new Color(0, 0, 0, 0.6f), 1);
-            cont.SetBorderRadius(3);
-            cont.SetPaddings(4);
-
-            var inspector = new IMGUIContainer(() =>
+            if(ve is IToolProvider)
             {
-                editor.OnInspectorGUI();
-            });
+                ((IToolProvider)ve).SetTools(toolkit);
+            }
 
-            cont.Add(inspector);
-            contentBehaviour.Add(cont);
-        }*/
+            toolkit.ChangeActive(0);
+
+            var content = new BehaviourContent(ve as LBSCustomEditor, behaviour.name, behaviour.icon, colorBH);
+            contentContainer.Add(content);
+
+        }
     }
 
     public override void Init(List<IManipulatorLBS> lBSManipulators, MainView view, LBSLayer layer, LBSBehaviour behaviour)
