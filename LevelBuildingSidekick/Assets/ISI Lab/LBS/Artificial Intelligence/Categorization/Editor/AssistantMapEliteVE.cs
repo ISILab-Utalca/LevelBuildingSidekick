0223c0397e3f098eb6f254dd9a75a4b0a9f4cf5f
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-08-08 10:28:33 -0400

    Update AssistantMapEliteVE.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Categorization/Editor/AssistantMapEliteVE.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Categorization/Editor/AssistantMapEliteVE.cs
@@ -1,25 +1,79 @@
+using LBS.Components;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 [CustomVisualElement(typeof(AssistantMapElite))]
-public class AssistantMapEliteVE : VisualElement
+public class AssistantMapEliteVE : LBSCustomEditor
 {
-    MapEliteWindow mapElites;
-    AssistantMapElite assitant;
+    MAPEliteConfiguration config;
+    MAPEliteContent content;
 
-    public AssistantMapEliteVE(LBSAssistantAI agent)
+    private object locker = new object();
+
+    public AssistantMapEliteVE(object target) : base(target)
     {
-        assitant = agent as AssistantMapElite;
-        var button = new Button(OpenAssitant);
-        button.text = "Map Elites";
-        Add(button);
+        Add(CreateVisualElement());
     }
 
-    public void OpenAssitant()
+    private void Init()
     {
-        var mapElites = MapEliteWindow.GetWindow<MapEliteWindow>();
-        mapElites.SetLayer(assitant.Owner);
+    }
+
+    private void Run()
+    {
+        var assitant = target as AssistantMapElite;
+        content.Empty();
+        assitant.LoadPresset(config.GetPresset());
+        assitant.SetAdam(new Rect()); // GET RECT FROM TOOL
+        assitant.Execute();
+    }
+
+    private void Continue()
+    {
+        var assitant = target as AssistantMapElite;
+        assitant.Continue();
+    }
+
+    private void UpdateContent()
+    {
+        var assitant = target as AssistantMapElite;
+        lock (locker)
+        {
+            content.UpdateContent();
+        }
+
+        if (assitant.Finished)
+        {
+            content.MarkEmpties();
+        }
+    }
+
+
+    public override void SetInfo(object target)
+    {
+    }
+
+    
+
+    protected override VisualElement CreateVisualElement()
+    {
+        var assistant = target as AssistantMapElite;
+
+        var ve = new VisualElement();
+        config = new MAPEliteConfiguration();
+        content = new MAPEliteContent(assistant);
+
+
+        config.OnCalculate += Run;
+        config.OnContinue += Continue;
+
+        content.OnSelectOption += assistant.ApplySuggestion;
+
+        ve.Add(content);
+        ve.Add(config);
+        return ve;
     }
 }
