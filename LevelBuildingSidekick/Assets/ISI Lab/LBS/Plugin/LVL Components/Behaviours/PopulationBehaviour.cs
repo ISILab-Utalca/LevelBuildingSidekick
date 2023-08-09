432a9fc3484c97d8b1cf07b6d171eb7414a3303a
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-08-09 17:21:17 -0400

    Update PopulationBehaviour.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Behaviours/PopulationBehaviour.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Behaviours/PopulationBehaviour.cs
@@ -13,17 +13,91 @@ public class PopulationBehaviour : LBSBehaviour
     [JsonIgnore]
     TileMapModule tileMap;
     [JsonIgnore]
-    BundleTileMap bundleTile;
+    BundleTileMap bundleTileMap;
+
     #endregion
 
-    #region CONSTRUCTORS
-    public PopulationBehaviour(Texture2D icon, string name) : base(icon, name) { }
-    #endregion
+    public BundleTileMap TileMap => bundleTileMap;
+
+    public PopulationBehaviour(Texture2D icon, string name) : base(icon, name)
+    {
+
+    }
 
     #region METHODS
+    public void CheckModules()
+    {
+        var tileMap = Owner.GetModule<TileMapModule>();
+        var bundleTileMap = Owner.GetModule<BundleTileMap>();
+
+        if (tileMap == null)
+        {
+            Debug.LogError("[ISILab] TileMap has been removed from Layer");
+            return;
+        }
+        if (bundleTileMap == null)
+        {
+            Debug.LogError("[ISILab] Connections Module has been removed from Layer");
+            return;
+        }
+
+        if (!tileMap.Equals(this.tileMap))
+        {
+            this.tileMap = tileMap;
+        }
+        if (!bundleTileMap.Equals(this.bundleTileMap))
+        {
+            this.bundleTileMap = bundleTileMap;
+        }
+    }
+
+    public void AddTile(Vector2Int position, Bundle bundle)
+    {
+        CheckModules();
+        var tile = new LBSTile(position, "Tile: " + position.ToString());
+        tileMap.AddTile(tile);
+        bundleTileMap.AddTile(tile, new BundleData(bundle));
+
+        //Calculate connections
+        //tileConnections.AddTile(tile, connections);
+    }
+
+    public void RemoveTile(Vector2Int position)
+    {
+        CheckModules();
+        var tile = tileMap.GetTile(position);
+        tileMap.RemoveTile(tile);
+        bundleTileMap.RemoveTile(tile);
+    }
+
+    public void SetBundle(LBSTile tile, Bundle bundle)
+    {
+        CheckModules();
+        var t = bundleTileMap.GetTile(tile);
+        t.BundleData = new BundleData(bundle);
+    }
+
+    public LBSTile GetTile(Vector2Int position)
+    {
+        CheckModules();
+        return tileMap.GetTile(position);
+    }
+
+    public BundleData GetBundleData(LBSTile tile)
+    {
+        CheckModules();
+        return bundleTileMap.GetTile(tile).BundleData;
+    }
+
+    public BundleData GetBundleData(Vector2 position)
+    {
+        CheckModules();
+        return GetBundleData(tileMap.GetTile(position.ToInt()));
+    }
+
     public override object Clone()
     {
-        throw new System.NotImplementedException();
+        return new PopulationBehaviour(this.Icon, this.Name);
     }
     #endregion
 }
