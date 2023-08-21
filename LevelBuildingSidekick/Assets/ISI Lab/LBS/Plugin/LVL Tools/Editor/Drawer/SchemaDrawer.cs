453cc7c046721b533f1b5117a36db970da7f1796
Nicolas Romero <nare.rare.fire@gmail.com>
2023-08-21 17:40:38 -0400

    Update SchemaDrawer.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Drawer/SchemaDrawer.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Drawer/SchemaDrawer.cs
@@ -3,6 +3,8 @@ using LBS.Components;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+using UnityEngine.EventSystems;
 
 [Drawer(typeof(SchemaBehaviour))]
 public class SchemaDrawer : Drawer
@@ -12,13 +14,25 @@ public class SchemaDrawer : Drawer
         throw new System.NotImplementedException();
     }
 
-    public override void Draw(LBSBehaviour behaviour, MainView view)
+    public override void Draw(object target, MainView view, Vector2 teselationSize)
     {
-        var schema = behaviour as SchemaBehaviour;
+        var schema = target as SchemaBehaviour;
 
-        foreach(var t in schema.Tiles)
+        foreach (var t in schema.Tiles)
         {
-            var v = new SchemaTileView(schema.GetConnections(t), schema.GetZone(t));
+            //var z = schema.GetZone(t);
+            //var c = schema.GetConnections(t);
+            var tView = new SchemaTileView();
+            var size = DefalutSize * teselationSize;
+            tView.SetPosition(new Rect(t.Position * size, size));
+
+            var zone = schema.GetZone(t);
+            tView.SetBackgroundColor(zone.Color);
+
+            var conections = schema.GetConnections(t);
+            tView.SetConnections(conections.ToArray());
+
+            view.AddElement(tView);
         }
     }
 }
