884ea7445e58ca2f82e85eb14834b994cc8a1ff7
Nicolas Romero <nare.rare.fire@gmail.com>
2023-08-21 17:40:31 -0400

    Update DrawTaggedTileMap.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Drawer/DrawTaggedTileMap.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Drawer/DrawTaggedTileMap.cs
@@ -24,7 +24,7 @@ public class DrawTaggedTileMap : Drawer
         }
     }
 
-    public override void Draw(LBSBehaviour behaviour, MainView view)
+    public override void Draw(object target, MainView view, Vector2 teselationSize)
     {
         throw new System.NotImplementedException();
     }
