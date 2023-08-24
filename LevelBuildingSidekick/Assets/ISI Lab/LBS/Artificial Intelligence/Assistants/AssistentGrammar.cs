29454466f9d03857b16c9cb8bcb0b6edfb0e48da
Nicolas Romero <nare.rare.fire@gmail.com>
2023-08-24 14:20:59 -0400

    Update AssistentGrammar.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Assistants/AssistentGrammar.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Assistants/AssistentGrammar.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 [System.Serializable]
-public class AssistentGrammar : LBSAssistantAI
+public class AssistentGrammar : LBSAssistant
 {
+    public AssistentGrammar(Texture2D icon, string name) : base(icon, name)
+    {
+    }
+
     public override object Clone()
     {
-        return new AssistentGrammar();
+        return new AssistentGrammar(this.Icon, this.Name);
     }
 
     public override void Execute()
