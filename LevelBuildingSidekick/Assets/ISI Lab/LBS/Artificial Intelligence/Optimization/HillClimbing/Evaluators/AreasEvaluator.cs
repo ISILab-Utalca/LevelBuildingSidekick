6d714ed50b4aa400c2bcbfc71cfaa8efd74e54ea
Nicolas Romero <nare.rare.fire@gmail.com>
2023-08-29 17:33:18 -0400

    Update AreasEvaluator.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Optimization/HillClimbing/Evaluators/AreasEvaluator.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Optimization/HillClimbing/Evaluators/AreasEvaluator.cs
@@ -1,37 +1,44 @@
 ﻿using Commons.Optimization.Evaluator;
+using LBS.Components;
 using LBS.Components.Graph;
 using LBS.Components.Specifics;
 using LBS.Components.TileMap;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class AreasEvaluator : IEvaluator
 {
-    LBSRoomGraph graph;
+    private LBSLayer original;
 
     Vector2 delta;
 
-    public AreasEvaluator() { }
-    public AreasEvaluator(LBSRoomGraph graph) 
+    public AreasEvaluator(LBSLayer layer) 
     {
-        this.graph = graph;
+        this.original = layer;
     }
 
-    private float EvaluateBySize(RoomNode node, TiledArea room)
+    private float EvaluateBySize(List<LBSModule> modules, Zone zone)
     {
+        var zones = modules.GetModule<SectorizedTileMapModule>();
+        var constrs = modules.GetModule<ConstrainsZonesModule>();
+
+        var bound = zones.GetBounds(zone);
+        var limit = constrs.GetLimits(zone);
+
         var vw = 1f;
-        if (room.Width != node.Room.Width)
+        if (bound.width > limit.maxWidth || bound.width < limit.minWidth)
         {
-            vw = room.Width / (float)node.Room.Width;
+            vw = bound.width / (float)limit.WidthMid;
             if (vw > 1)
                 vw = 1 / vw;
         }
 
         var vh = 1f;
-        if (room.Height != node.Room.Height)
+        if (bound.height > limit.maxHeight || bound.height < limit.minHeight)
         {
-            vh = room.Height / (float)node.Room.Height;
+            vh = bound.height / (float)limit.WidthMid;
             if (vh > 1)
                 vh = 1 / vh;
         }
@@ -41,16 +48,21 @@ public class AreasEvaluator : IEvaluator
 
     public float Evaluate(IOptimizable evaluable)
     {
-        var schema = (evaluable as OptimizableSchema).Schema;
-        var value = 0f;
-        for (int i = 0; i < graph.NodeCount; i++)
-        {
-            var node = graph.GetNode(i);
-            var room = schema.GetArea(node.ID);
+        var modules = (evaluable as OptimizableModules).Modules;
 
-            value += EvaluateBySize(node, room);
+        var zones = modules.GetModule<SectorizedTileMapModule>();
+        var connected = modules.GetModule<ConnectedZonesModule>();
+
+        var value = 0f;
+
+
+        for (int i = 0; i < zones.ZonesWithTiles.Count; i++)
+        {
+            var zone = zones.ZonesWithTiles[i];
+
+            value += EvaluateBySize(modules, zone);
         }
-        return value / (schema.AreaCount * 1f);
+        return value / (zones.ZonesWithTiles.Count * 1f);
     }
 
 
