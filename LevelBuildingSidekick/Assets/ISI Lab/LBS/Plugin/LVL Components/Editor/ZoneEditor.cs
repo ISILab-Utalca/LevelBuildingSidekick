290b81e03fe13397dbb5ee838dbf159f0fb6e029
Nicolas Romero <nare.rare.fire@gmail.com>
2023-09-06 16:32:00 -0300

    Update ZoneEditor.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Editor/ZoneEditor.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Components/Editor/ZoneEditor.cs
@@ -3,6 +3,7 @@ using LBS.Components.TileMap;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -15,6 +16,7 @@ public class ZoneEditor : LBSCustomEditor
 
     private TextField nameField;
     private ColorField colorField;
+    private ObjectField objectField;
 
     private ListView bundleList;
     private List<string> bundlesRef; // ?
@@ -26,10 +28,20 @@ public class ZoneEditor : LBSCustomEditor
 
     public override void SetInfo(object target)
     {
+        // Set referenced Zone
         this.target = target as Zone;
 
+        // Get bundles
+        var bundles = LBSAssetsStorage.Instance.Get<Bundle>();
+
+        // Set basic values
         nameField.value = this.target.ID;
         colorField.value = this.target.Color;
+
+        // Set parche value
+        if (this.target.TagsBundles.Count > 0)
+            objectField.value = bundles.First(b => b.ID.Label == this.target.TagsBundles[0]);
+
         bundlesRef = this.target.TagsBundles;
     }
 
@@ -51,12 +63,24 @@ public class ZoneEditor : LBSCustomEditor
         });
         this.Add(colorField);
 
-        //BundleList
+        // ObjectField (Bundle)
+        this.objectField = new ObjectField("Bundle");
+        this.objectField.objectType = typeof(Bundle);
+        objectField.RegisterValueChangedCallback(v => // objectField.RegisterCallback<ChangeEvent<ScriptableObject>>(evt => 
+        {
+            var bundle = v.newValue as Bundle;
+            target.TagsBundles = new List<string>() { bundle.ID.Label };
+        });
+        this.Add(objectField);
+
+        // BundleList
+        /*
         this.bundleList = new ListView();
         bundleList.itemsSource = bundlesRef;
         bundleList.makeItem = MakeItem;
         bundleList.bindItem = BindItem;
         this.Add(bundleList);
+        */
 
         return this;
 
@@ -71,7 +95,12 @@ public class ZoneEditor : LBSCustomEditor
 
     private void BindItem(VisualElement ve, int index)
     {
+        var field = (ve as ObjectField);
 
+        var bundles = LBSAssetsStorage.Instance.Get<Bundle>();
+        bundles = bundles.Where(b=> b.ID.Label == bundlesRef[index]).ToList();
+
+        field.value = bundles[index];
     }
 
 }
