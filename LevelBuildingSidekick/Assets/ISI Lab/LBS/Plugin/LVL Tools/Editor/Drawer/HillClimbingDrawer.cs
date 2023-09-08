8588ea1a0e70cfa70238038bef59d2a5089e9101
Nicolas Romero <nare.rare.fire@gmail.com>
2023-09-08 17:56:42 -0300

    Update HillClimbingDrawer.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Drawer/HillClimbingDrawer.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Drawer/HillClimbingDrawer.cs
@@ -1,6 +1,8 @@
 using LBS.Components;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,11 +13,6 @@ public class HillClimbingDrawer : Drawer
 
     private Dictionary<Zone, LBSNodeView> viewRefs = new();
 
-    public override void Draw(ref LBSLayer layer, MainView view)
-    {
-        throw new System.NotImplementedException();
-    }
-
     public override void Draw(object target, MainView view, Vector2 teselationSize)
     {
         // clear preview view refrences
@@ -24,22 +21,35 @@ public class HillClimbingDrawer : Drawer
         // Set target Assitant
         var assistant = target as HillClimbingAssistant;
 
-        // Get zones
+        // Get modules
+        var consts = assistant.ConstrainsZonesMod.Constraints;
         var zones = assistant.ZonesWhitTiles;
+
         List<LBSNodeView> nViews = new();
+        List<DottedAreaFeedback> cViews = new();
         foreach (var zone in zones)
         {
             // Create node view
             var nView = new LBSNodeView();
 
-            var bound = assistant.GetTiles(zone).GetBounds();
+            var tiles = assistant.GetTiles(zone);
+            var bound = tiles.GetBounds();
 
             // Set view values
             nView.SetPosition(new Rect(bound.center * nodeSize - (nodeSize / 2f), nodeSize));
             nView.SetText(zone.ID);
             nView.SetColor(zone.Color);
 
+            // add node view to list
             nViews.Add(nView);
+
+            /*
+            if (consts.Count > 0) // parche
+            {
+                var pair = consts.First(p => p.zone == zone);
+                cViews.AddRange(CreateFeedBackAreas(pair, bound.center, teselationSize));
+            }
+            */
 
             // Add to reference dictionary
             viewRefs.Add(zone, nView);
@@ -61,7 +71,33 @@ public class HillClimbingDrawer : Drawer
             view.AddElement(eView);
         }
 
+        // print everything
         eViews.ForEach(e => view.AddElement(e));
+        cViews.ForEach(c => view.AddElement(c));
         nViews.ForEach(n => view.AddElement(n));
+    }
+
+    private List<DottedAreaFeedback> CreateFeedBackAreas(ConstraintPair pair,Vector2 center, Vector2 size)
+    {
+        List<DottedAreaFeedback> cViews = new();
+
+        var constr = pair.Constraint;
+
+        var maxV1 = new Vector2(-constr.maxHeight / 2f, -constr.maxWidth / 2f);
+        var maxV2 = new Vector2(constr.maxHeight / 2f, constr.maxWidth / 2f);
+        var c1 = new DottedAreaFeedback();
+        c1.ActualizePositions(((maxV1 + center) * (size * 100)).ToInt(),(( maxV2 + center) * (size * 100)).ToInt());
+        c1.SetColor(Color.red);
+
+        var minV1 = new Vector2(-constr.minHeight / 2f, -constr.minWidth / 2f);
+        var minV2 = new Vector2(constr.minHeight / 2f, constr.minWidth / 2f);
+        var c2 = new DottedAreaFeedback();
+        c2.ActualizePositions(minV1.ToInt(),minV2.ToInt());
+
+        // add constraint to list
+        cViews.Add(c1);
+        cViews.Add(c2);
+
+        return cViews;
     }
 }
