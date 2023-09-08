03bbd60683f667f74343f8e205bf5487c605ea97
Nicolas Romero <nare.rare.fire@gmail.com>
2023-09-08 17:56:26 -0300

    Update DrawGrammarGraph.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Drawer/DrawGrammarGraph.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Drawer/DrawGrammarGraph.cs
@@ -10,36 +10,6 @@ public class DrawGrammarGraph : Drawer
 {
     public DrawGrammarGraph() : base() { }
 
-    public override void Draw(ref LBSLayer layer, MainView view)
-    {
-        /*
-        var graph = layer.GetModule<LBSGraph>();
-
-        var nViews = new List<LBSNodeView_Old<LBSNode>>();
-        foreach (var node in graph.GetNodes())
-        {
-            var size = new Vector2(300, 100);
-            var element = new LBSNodeView_Old<LBSNode>(node, node.Position - (size / 2f), size);
-
-            element.SetText(node.ID);
-            //element.SetColor(node.Room.Color);
-
-            nViews.Add(element);
-            view.AddElement(element);
-        }
-
-        foreach (var edge in graph.GetEdges())
-        {
-            var n1 = nViews.Find(v => v.Data.Equals(edge.FirstNode));
-            var n2 = nViews.Find(v => v.Data.Equals(edge.SecondNode));
-            var element = new LBSEdgeView<LBSEdge, LBSNode>(edge, n1, n2, 10, 3);
-            view.AddElement(element);
-            element.SendToBack();
-        }
-        */
-    }
-
-
     public override void Draw(object target, MainView view, Vector2 teselationSize)
     {
         throw new System.NotImplementedException();
