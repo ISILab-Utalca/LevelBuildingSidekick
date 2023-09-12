398a681a8777038b562ad5cac0a9875c107133ab
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-09-12 11:06:00 -0300

    Update AssistantMapEliteVE.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Assistants/Editor/AssistantMapEliteVE.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Assistants/Editor/AssistantMapEliteVE.cs
@@ -1,34 +1,44 @@
 using LBS.Components;
+using LBS.VisualElements;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 [LBSCustomEditor("AssistantMapElite", typeof(AssistantMapElite))]
-public class AssistantMapEliteVE : LBSCustomEditor
+public class AssistantMapEliteVE : LBSCustomEditor, IToolProvider
 {
     MAPEliteConfiguration config;
     MAPEliteContent content;
 
     private object locker = new object();
 
+    public Rect Rect { get; set; }
+
+    ActOnRect ActOnRect;
+
     public AssistantMapEliteVE(object target) : base(target)
     {
         Add(CreateVisualElement());
-    }
-
-    private void Init()
-    {
+        SetInfo(target);
     }
 
     private void Run()
     {
         var assitant = target as AssistantMapElite;
-        content.Empty();
+        content.Reset();
         assitant.LoadPresset(config.GetPresset());
-        assitant.SetAdam(new Rect()); // GET RECT FROM TOOL
+        assitant.SetAdam(Rect); // GET RECT FROM TOOL
         assitant.Execute();
+    }
+
+    public void ChangePresset()
+    {
+        var assitant = target as AssistantMapElite;
+        assitant.LoadPresset(config.GetPresset());
+        content.Reset();
     }
 
     private void Continue()
@@ -69,11 +79,22 @@ public class AssistantMapEliteVE : LBSCustomEditor
 
         config.OnCalculate += Run;
         config.OnContinue += Continue;
+        config.OnPressetChange += (p) => ChangePresset();
 
         content.OnSelectOption += assistant.ApplySuggestion;
 
         ve.Add(content);
         ve.Add(config);
         return ve;
+    }
+
+    public void SetTools(ToolKit toolkit)
+    {
+        var assitant = target as AssistantMapElite;
+        var icon = Resources.Load<Texture2D>("Icons/Select");
+        ActOnRect = new ActOnRect((r) => Rect = r);
+        var t1 = new LBSTool(icon, "Paint Zone", ActOnRect);
+        t1.Init(assitant.Owner, assitant);
+        toolkit.AddTool(t1);
     }
 }
