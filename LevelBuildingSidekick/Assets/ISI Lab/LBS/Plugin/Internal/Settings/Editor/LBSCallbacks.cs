4547641cf0f9185b6437571f7ab68257c8312cf1
Nicolas Romero <nare.rare.fire@gmail.com>
2023-10-03 18:46:04 -0300

    Update LBSCallbacks.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Internal/Settings/Editor/LBSCallbacks.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Internal/Settings/Editor/LBSCallbacks.cs
@@ -15,32 +15,101 @@ public class LBSCallbacks
 {
     static LBSCallbacks()
     {
-
         var onStart = SessionState.GetBool("start", true);
         if (onStart)
         {
             EditorApplication.update += OnStartEditor;
             SessionState.SetBool("start", false);
         }
+
+        AssemblyReloadEvents.afterAssemblyReload += OnAfterReloadScript;
+        AssemblyReloadEvents.beforeAssemblyReload += OnBeforeReloadScript;
     }
 
-    private static void OnStartEditor() // <----
+    /// <summary>
+    /// called when the editor starts for the first time
+    /// </summary>
+    private static void OnStartEditor()
     {
-        // busca y setea la instancia de "LBS Settings" en su singleton
         SettingsEditor.SearchSettingsInstance();
         ReloadBundles();
 
         EditorApplication.update -= OnStartEditor;
-
     }
 
-
-    [UnityEditor.Callbacks.DidReloadScripts()]
-    private static void OnScriptsReloaded() // <----
+    /// <summary>
+    /// called before the script is reloaded
+    /// </summary>
+    private static void OnBeforeReloadScript()
     {
-        //ReloadStorage();
+        SaveBackUp();
+    }
+
+    /// <summary>
+    /// called after the script is reloaded
+    /// </summary>
+    private static void OnAfterReloadScript()
+    {
+        LoadBackUp();
         ReloadCurrentLevel();
-        //ReloadBundles();
+    }
+
+    /// <summary>
+    /// save the level in the backup temporarily
+    /// </summary>
+    private static void SaveBackUp()
+    {
+        var level = LBS.LBS.loadedLevel;
+        if(level != null)
+        {
+            var settings = LBSSettings.Instance;
+            var path = settings.paths.backUpPath;
+            var folderPath = Path.GetDirectoryName(path);
+
+            var backUp = ScriptableObject.CreateInstance<BackUp>();
+            backUp.level = level;
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            AssetDatabase.CreateAsset(backUp, path);
+            AssetDatabase.SaveAssets();
+
+            Debug.Log("[ISI Lab]: Save on BackUp");
+        }
+        else
+        {
+            Debug.Log("[ISI Lab]: Error on save BackUp");
+        }
+    }
+
+    /// <summary>
+    /// Load the level from the backup and delete it
+    /// </summary>
+    private static void LoadBackUp()
+    {
+        // search and set the instance of "LBS Settings" in its singleton
+        var settings = LBSSettings.Instance;
+        var path = settings.paths.backUpPath;
+        var backUp = AssetDatabase.LoadAssetAtPath<BackUp>(path);
+
+        if (backUp != null)
+        {
+            // load the level from the backup
+            LBS.LBS.loadedLevel = backUp.level;
+
+            // After loading the level, the backup is destroyed
+            AssetDatabase.DeleteAsset(path);
+            Debug.Log("[ISI Lab]: Load from BackUp");
+        }
+        else
+        {
+            // if the backup is not found, a new level is created
+            LBS.LBS.loadedLevel = new LoadedLevel(new LBSLevelData(), "New level");
+            Debug.Log("[ISI Lab]: Error on load BackUp");
+        }
     }
 
     public static void ReloadStorage()
@@ -51,7 +120,7 @@ public class LBSCallbacks
 
     public static void ReloadCurrentLevel()
     {
-        var data = LBSController.CurrentLevel.data;
+        var data = LBS.LBS.loadedLevel.data;
         data.Reload();
     }
 
@@ -66,5 +135,3 @@ public class LBSCallbacks
     }
 
 }
-
-
