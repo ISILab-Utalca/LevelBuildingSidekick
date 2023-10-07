f11fac45a12c837505fd175438779ba0df36627a
Nicolas Romero <nare.rare.fire@gmail.com>
2023-10-07 20:41:03 -0300

    changes

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Generators3D/ExteriorRuleGenerator.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Generators3D/ExteriorRuleGenerator.cs
@@ -42,6 +42,8 @@ namespace LBS.Generator
             // Get CharacteristicGroup bundles
             bundles = bundles.Where(b => b.GetCharacteristics<LBSDirectionedGroup>().Count > 0 && !b.IsPresset).ToList();
 
+            bundles[0].ChildsBundles.ForEach(b => b.Characteristics.ForEach(c => c.Init(b))); // PARCHE (!)
+
             var selected = bundles[0].GetCharacteristics<LBSDirectionedGroup>()[0];
 
             // Create pivot
@@ -59,7 +61,7 @@ namespace LBS.Generator
 
                 // Get pref
                 var pair = GetBundle(selected, con.ToArray());
-                var pref = pair.Item1.Weights.RandomRullete(w => w.weigth).target;
+                var pref = pair.Item1.Owner.Assets.RandomRullete(w => w.probability).obj;
 
 #if UNITY_EDITOR
                 var go = PrefabUtility.InstantiatePrefab(pref, mainPivot.transform) as GameObject;
@@ -67,7 +69,7 @@ namespace LBS.Generator
                 var go = GameObject.Instantiate(pref, mainPivot.transform);
 #endif
 
-                go.transform.position = new Vector3((tile.Position.x) * scale.x, 0, (tile.Position.y) * scale.y) + new Vector3(scale.x, 0, scale.y) / 2;
+                go.transform.position = new Vector3((tile.Position.x) * scale.x, 0, (tile.Position.y) * scale.y) + new Vector3(scale.x, 0, scale.y) / 2f;
 
                 if (pair.Item2 % 2 == 0)
                     go.transform.rotation = Quaternion.Euler(0, 90 * (pair.Item2) % 360, 0);
