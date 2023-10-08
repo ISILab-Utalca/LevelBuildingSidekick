4b5226c4291be4b35b135f6ca0c1a68076b5f864
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2023-10-07 21:39:39 -0300

    RTSPresset

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Optimization/EvolutionaryAlgorithm/Evaluators/RTS/Exploration.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Optimization/EvolutionaryAlgorithm/Evaluators/RTS/Exploration.cs
@@ -40,6 +40,63 @@ public class Exploration : IRangedEvaluator
             if (!(g as BundleData).Characteristics.Contains(colliderCharacteristic))
                 fitness++;
         }
+        /*
+        var dir = Directions.Bidimencional.Edges;
+        List<int> closed = new List<int>();
+        List<int> open = new List<int>();
+
+        int breaks = -1;
+
+        for(int i = 0; i < genes.Count; i++)
+        {
+            if (closed.Contains(i))
+                continue;
+
+            if (genes[i] != null)
+            {
+                if ((genes[i] as BundleData).Characteristics.Contains(colliderCharacteristic))
+                {
+                    closed.Add(i);
+                    continue;
+                }
+            }
+
+            open.Add(i);
+
+            while(open.Count > 0)
+            {
+                var parent = open[0];
+                open.RemoveAt(0);
+                var pos = chrom.ToMatrixPosition(i);
+
+                foreach(var d in dir)
+                {
+                    var p = pos + d;
+                    var child = chrom.ToIndex(p);
+
+                    if (closed.Contains(child) || open.Contains(child))
+                        continue;
+
+                    if(child < 0 || child >= chrom.Length)
+                        continue;
+
+                    if (genes[child] != null)
+                    {
+                        if ((genes[child] as BundleData).Characteristics.Contains(colliderCharacteristic))
+                        {
+                            closed.Add(child);
+                        }
+                    }
+
+                    open.Add(child);
+                }
+
+                closed.Add(parent);
+                fitness++;
+
+            }
+            breaks++;
+        }*/
 
         fitness /= (float)genes.Count;
 
