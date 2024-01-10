b11963e68edc08d4d31c3e404f35324467be7707
Nicolas Romero <nare.rare.fire@gmail.com>
2024-01-10 10:42:03 -0300

    ADD timers debug

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Optimization/HillClimbing/HillClimbing.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Optimization/HillClimbing/HillClimbing.cs
@@ -24,6 +24,9 @@ namespace ISILab.AI.Optimization
         public double NNlog = 0;
         public double Elog = 0;
 
+        public float _nbrsTimer = 0;
+        public float _fitTimer = 0;
+
         public HillClimbing(IPopulation population, IEvaluator evaluator, ISelection selection, Func<IOptimizable, List<IOptimizable>> getNeighbors,  ITermination termination) : base( population, evaluator, selection, termination)
         {
             GetNeighbors = getNeighbors;
@@ -42,6 +45,8 @@ namespace ISILab.AI.Optimization
             var clock = new Stopwatch();
 
             var last = Population.Generations.Last();
+            
+            clock.Restart();
             var selection = this.Selection.SelectEvaluables(1, last);
 
             if(selection.Count == 0)
@@ -51,12 +56,14 @@ namespace ISILab.AI.Optimization
 
             var best = selection.First();
 
+
             if (GetNeighbors == null)
                 throw new NullReferenceException();
 
             clock.Restart();
             var offsprings = GetNeighbors.Invoke(best);
             clock.Stop();
+            _nbrsTimer = clock.ElapsedMilliseconds / 1000f;
 
             Nlog = clock.ElapsedMilliseconds;
             NNlog= offsprings.Count;
@@ -65,6 +72,8 @@ namespace ISILab.AI.Optimization
             clock.Restart();
             EvaluateFitness(offsprings);
             clock.Stop();
+            _fitTimer = clock.ElapsedMilliseconds / 1000f;
+
             Elog = clock.ElapsedMilliseconds;
 
             Population.CreateNewGeneration(offsprings);
@@ -78,7 +87,9 @@ namespace ISILab.AI.Optimization
 
         public override void PrintClocks()
         {
-            UnityEngine.Debug.Log("CLOCKS");
+            UnityEngine.Debug.Log("Neighbors: " + _nbrsTimer + "s.");
+            UnityEngine.Debug.Log("Fitness: " + _fitTimer + "s.");
+
         }
     }
 }
