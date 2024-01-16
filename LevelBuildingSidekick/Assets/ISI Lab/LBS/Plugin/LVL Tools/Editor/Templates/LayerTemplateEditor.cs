3d017dcf29d3c06934ba6519e500f48e32e069f2
Gabriel Sepúlveda <43820153+Prion6@users.noreply.github.com>
2024-01-16 11:57:47 -0300

    Remove Quest from layers

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Templates/LayerTemplateEditor.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/LVL Tools/Editor/Templates/LayerTemplateEditor.cs
@@ -85,11 +85,6 @@ public class LayerTemplateEditor : Editor
         {
             PopulationConstruct(template);
         }
-
-        if (GUILayout.Button("Set as Quest"))
-        {
-            Questconstuct(template);
-        }
     }
 
     /// <summary>
@@ -239,55 +234,4 @@ public class LayerTemplateEditor : Editor
         AssetDatabase.SaveAssets();
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="template"></param>
-    private void Questconstuct(LayerTemplate template)
-    {
-        template.Clear();
-
-        // Basic data layer
-        var layer = template.layer;
-        layer.ID = "Quest";
-        layer.Name = "Layer Quest";
-        layer.iconPath = "Assets/ISI Lab/LBS/Plugin/Assets2D/Resources/Icons/Quest.png";
-        template.layer = layer;
-
-        layer.Settings = new Generator3D.Settings()
-        {
-            scale = new Vector2Int(2, 2),
-            resize = new Vector2(0, 0),
-            position = new Vector3(0, 0, 0),
-            name = "Quest",
-        };
-
-        var bhIcon = Resources.Load<Texture2D>("Icons/Select");
-        //var bh = new QuestBehaviour(bhIcon, "Quest behaviour");
-        //layer.AddBehaviour(bh);
-
-
-        //layer.AddAssistant(new AssistentGrammar());
-
-        // Modules
-        //layer.AddModule(new LBSGraph());
-        //layer.AddModule(new LBSGrammarGraph()); // (!)
-
-        // Mode 1
-        /*
-        Texture2D icon = Resources.Load<Texture2D>("Icons/Select");
-        var tool1 = new LBSTool(icon, "Select", typeof(Select), null, true);
-        icon = Resources.Load<Texture2D>("Icons/Addnode");
-        var tool2 = new LBSTool(icon, "Add node", typeof(CreateNewGrammarNode), typeof(GrammarPallete), false); // (!)
-        icon = Resources.Load<Texture2D>("Icons/AddConnection");
-        var tool3 = new LBSTool(icon, "Add conection", typeof(CreateNewConnection<LBSNode>), null, false); // (!)
-        icon = Resources.Load<Texture2D>("Icons/Trash");
-        var tool4 = new LBSTool(icon, "Remove", typeof(RemoveGraphNode<LBSNode>), null, false); // (!)
-        */
-        //template.modes.Add(mode1);
-
-        Debug.Log("Set Quest Default");
-        EditorUtility.SetDirty(target);
-        AssetDatabase.SaveAssets();
-    }
 }
