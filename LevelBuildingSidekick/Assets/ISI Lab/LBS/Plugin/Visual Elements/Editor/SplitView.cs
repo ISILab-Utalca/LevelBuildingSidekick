a52ef1ca8f4b049e168c3f1108e2f119928ab261
Nicolas Romero <nare.rare.fire@gmail.com>
2024-01-25 16:47:44 -0300

    Update SplitView.cs

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/SplitView.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Visual Elements/Editor/SplitView.cs
@@ -10,23 +10,15 @@ public class SplitView : TwoPaneSplitView
 {
     public new class UxmlFactory : UxmlFactory<SplitView, TwoPaneSplitView.UxmlTraits> { }
 
-    private int minWidth = 45;
-
-    private VisualElement dragLineAnchor;
-    private VisualElement dragLine;
+    //private VisualElement dragLineAnchor;
+    //private VisualElement dragLine;
 
     public SplitView()
     {
         var content = this.Q<VisualElement>("unity-content-container");
         content.pickingMode = PickingMode.Ignore;
 
-        dragLineAnchor = this.Q<VisualElement>("unity-dragline-anchor");
-        dragLine = this.Q<VisualElement>("unity-dragline");
+        var dragLineAnchor = this.Q<VisualElement>("unity-dragline-anchor");
+        var dragLine = this.Q<VisualElement>("unity-dragline");
     }
-
-    public void OnAttach(AttachToPanelEvent evt)
-    {
-
-    }
-
 }
