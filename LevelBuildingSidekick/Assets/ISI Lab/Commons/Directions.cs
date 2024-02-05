6480c18ec3416fc659759c8e01aebe3513155419
Nicolas Romero <nare.rare.fire@gmail.com>
2024-02-05 17:23:06 -0300

    Update Directions.cs

--- LevelBuildingSidekick/Assets/ISI Lab/Commons/Directions.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/Commons/Directions.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Directions
+namespace ISILab.Commons
 {
-    /// <summary>
-    /// A static class that provides predefined lists of 3D vectors
-    /// representing centers, edges, and diagonals in a 3D space.
-    /// </summary>
-    public static class Tridimencional
+
+    public static class Directions
     {
-        private static readonly List<Vector3Int> centers = new List<Vector3Int>()
+        /// <summary>
+        /// A static class that provides predefined lists of 3D vectors
+        /// representing centers, edges, and diagonals in a 3D space.
+        /// </summary>
+        public static class Tridimencional
+        {
+            private static readonly List<Vector3Int> centers = new List<Vector3Int>()
         {
             new Vector3Int(1,0,0),
             new Vector3Int(0,1,0),
@@ -21,7 +24,7 @@ public static class Directions
             new Vector3Int(0,0,-1),
         };
 
-        private static readonly List<Vector3Int> diagonals = new List<Vector3Int>()
+            private static readonly List<Vector3Int> diagonals = new List<Vector3Int>()
         {
             new Vector3Int(1, 1, 1),
             new Vector3Int(-1, 1, 1),
@@ -33,7 +36,7 @@ public static class Directions
             new Vector3Int(-1, -1, -1)
         };
 
-        private static readonly List<Vector3Int> edges = new List<Vector3Int>()
+            private static readonly List<Vector3Int> edges = new List<Vector3Int>()
         {
             new Vector3Int(1, 1, 0),
             new Vector3Int(1, -1, 0),
@@ -49,100 +52,101 @@ public static class Directions
             new Vector3Int(0, -1, -1)
         };
 
-        public static List<Vector3Int> Centers => centers;
-        public static List<Vector3Int> Edges => edges;
-        public static List<Vector3Int> Diagonals => diagonals;
-        public static List<Vector3Int> All
-        {
-            get
+            public static List<Vector3Int> Centers => centers;
+            public static List<Vector3Int> Edges => edges;
+            public static List<Vector3Int> Diagonals => diagonals;
+            public static List<Vector3Int> All
             {
-                var all = new List<Vector3Int>();
-                all.AddRange(centers);
-                all.AddRange(edges);
-                all.AddRange(diagonals);
-                return all;
-            }
-        }
-
-        /// <summary>
-        /// Finds the perpendicular directions to a given axis.
-        /// </summary>
-        /// <param name="axis">The axis for which to find the perpendicular directions.</param>
-        /// <returns>Perpendicular directions to the axis.</returns>
-        public static List<Vector3Int> GetPerpendicularDirs(Vector3Int axis, bool whitDiagonals = false)
-        {
-            var toR = new List<Vector3Int>();
-
-            foreach (var direction in Centers)
-            {
-                if (Vector3.Dot(axis, direction) == 0)
+                get
                 {
-                    toR.Add(direction);
+                    var all = new List<Vector3Int>();
+                    all.AddRange(centers);
+                    all.AddRange(edges);
+                    all.AddRange(diagonals);
+                    return all;
                 }
             }
 
-            if (!whitDiagonals)
+            /// <summary>
+            /// Finds the perpendicular directions to a given axis.
+            /// </summary>
+            /// <param name="axis">The axis for which to find the perpendicular directions.</param>
+            /// <returns>Perpendicular directions to the axis.</returns>
+            public static List<Vector3Int> GetPerpendicularDirs(Vector3Int axis, bool whitDiagonals = false)
+            {
+                var toR = new List<Vector3Int>();
+
+                foreach (var direction in Centers)
+                {
+                    if (Vector3.Dot(axis, direction) == 0)
+                    {
+                        toR.Add(direction);
+                    }
+                }
+
+                if (!whitDiagonals)
+                    return toR;
+
+                foreach (var direction in Edges)
+                {
+                    if (Vector3.Dot(axis, direction) == 0)
+                    {
+                        toR.Add(direction);
+                    }
+                }
+
                 return toR;
-
-            foreach (var direction in Edges)
-            {
-                if (Vector3.Dot(axis, direction) == 0)
-                {
-                    toR.Add(direction);
-                }
             }
-
-            return toR;
         }
-    }
-
-    /// <summary>
-    /// A static class that provides predefined lists of 2D vectors 
-    /// representing edges, diagonals, and all directions.
-    /// </summary>
-    public static class Bidimencional
-    {
-        private readonly static List<Vector2Int> edges = new List<Vector2Int>()
-        {
-            new Vector2Int(1, 0),    // Derecha
-            new Vector2Int(0, 1),    // Arriba
-            new Vector2Int(-1, 0),   // Izquierda
-            new Vector2Int(0, -1),   // Abajo
-        };
-
-        private readonly static List<Vector2Int> diagonals = new List<Vector2Int>()
-        {
-            new Vector2Int(1, 1),    // Diagonal superior derecha
-            new Vector2Int(-1, 1),   // Diagonal superior izquierda
-            new Vector2Int(-1, -1),  // Diagonal inferior izquierda
-            new Vector2Int(1, -1)    // Diagonal inferior derecha
-        };
-
-        private readonly static List<Vector2Int> all = new List<Vector2Int>()
-        {
-            new Vector2Int(1, 0),     // Derecha
-            new Vector2Int(1, 1),      // Diagonal superior derecha
-            new Vector2Int(0, 1),     // Arriba
-            new Vector2Int(-1, 1),    // Diagonal superior izquierda
-            new Vector2Int(-1, 0),    // Izquierda
-            new Vector2Int(-1, -1),   // Diagonal inferior izquierda
-            new Vector2Int(0, -1),    // Abajo
-            new Vector2Int(1, -1)    // Diagonal inferior derecha
-        };
 
         /// <summary>
-        /// List of vectors representing edges.
+        /// A static class that provides predefined lists of 2D vectors 
+        /// representing edges, diagonals, and all directions.
         /// </summary>
-        public static List<Vector2Int> Edges => edges;
+        public static class Bidimencional
+        {
+            private readonly static List<Vector2Int> edges = new List<Vector2Int>()
+        {
+            new Vector2Int(1, 0),    // Right
+            new Vector2Int(0, 1),    // Up
+            new Vector2Int(-1, 0),   // Left
+            new Vector2Int(0, -1),   // Down
+        };
 
-        /// <summary>
-        /// List of vectors representing diagonals.
-        /// </summary>
-        public static List<Vector2Int> Diagonals => diagonals;
+            private readonly static List<Vector2Int> diagonals = new List<Vector2Int>()
+        {
+            new Vector2Int(1, 1),    // Diagonal Upper Right
+            new Vector2Int(-1, 1),   // Diagonal Upper Left
+            new Vector2Int(-1, -1),  // Diagonal Lower Left
+            new Vector2Int(1, -1)    // Diagonal Lower Right
+        };
 
-        /// <summary>
-        /// List of vectors representing all directions.
-        /// </summary>
-        public static List<Vector2Int> All => all;
+            private readonly static List<Vector2Int> all = new List<Vector2Int>()
+        {
+            new Vector2Int(1, 0),   // Right
+            new Vector2Int(1, 1),   // Diagonal Upper Right
+            new Vector2Int(0, 1),   // Up
+            new Vector2Int(-1, 1),  // Diagonal Upper Left
+            new Vector2Int(-1, 0),  // Left
+            new Vector2Int(-1, -1), // Diagonal Lower Left
+            new Vector2Int(0, -1),  // Down
+            new Vector2Int(1, -1)   // Diagonal Lower Right
+        };
+
+            /// <summary>
+            /// List of vectors representing edges.
+            /// </summary>
+            public static List<Vector2Int> Edges => edges;
+
+            /// <summary>
+            /// List of vectors representing diagonals.
+            /// </summary>
+            public static List<Vector2Int> Diagonals => diagonals;
+
+            /// <summary>
+            /// List of vectors representing all directions.
+            /// </summary>
+            public static List<Vector2Int> All => all;
+        }
     }
 }
