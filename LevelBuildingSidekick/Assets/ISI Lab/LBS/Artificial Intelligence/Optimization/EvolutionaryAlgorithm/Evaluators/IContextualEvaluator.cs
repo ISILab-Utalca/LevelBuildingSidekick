33d48862996f8ee2cf222fd8e7112843e79504ce
RosenkreuzAlice <74314360+RosenkreuzAlice@users.noreply.github.com>
2025-07-10 14:22:41 -0400

    Context Evaluator Update

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Optimization/EvolutionaryAlgorithm/Evaluators/IContextualEvaluator.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Artificial Intelligence/Optimization/EvolutionaryAlgorithm/Evaluators/IContextualEvaluator.cs
@@ -1,7 +1,11 @@
 using Commons.Optimization.Evaluator;
+using ISILab.LBS.Behaviours;
+using ISILab.LBS.Modules;
 using LBS.Components;
+using LBS.Components.TileMap;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public interface IContextualEvaluator : IEvaluator
@@ -12,6 +16,84 @@ public interface IContextualEvaluator : IEvaluator
 
     public LBSLayer InteriorLayers()
     {
-        throw new NotImplementedException();
+        if (ContextLayers.Count == 0) return null;
+
+        //If there's no schema layers, return null again. This might be the last failsafe we need!
+        var interiorLayers = ContextLayers.FindAll(l => l.Behaviours.Any(b => b.GetType().Equals(typeof(SchemaBehaviour))));
+        if (interiorLayers.Count == 0) return null;
+        
+        //Clone first
+        var combinedLayer = interiorLayers.First().Clone() as LBSLayer;
+
+        //Now we check everything
+        foreach (LBSLayer interiorLayer in interiorLayers)
+        {
+            //Skip the one we just cloned
+            if (interiorLayer.Equals(combinedLayer)) continue;
+            //Get behaviors
+            var tempBehavior = interiorLayer.Behaviours.Find(b => b.GetType().Equals(typeof(SchemaBehaviour))) as SchemaBehaviour;
+            var combinedBehavior = combinedLayer.Behaviours.Find(b => b.GetType().Equals(typeof(SchemaBehaviour))) as SchemaBehaviour;
+
+            foreach(LBSTile tile in tempBehavior.Tiles) 
+            {
+                //If there's already a tile here, return. We'll ignore everything that overlaps.
+                //The warning is there for a reason!
+                if (combinedBehavior.GetTile(tile.Position) != null) continue;
+                //Get zone from the tile we're looking at
+                var zone = tempBehavior.GetZone(tile);
+                //If the zone isn't in the tilemap, add it
+                if(!combinedBehavior.Zones.Contains(zone))
+                {
+                    combinedBehavior.Zones.Add(zone);
+                }
+                //Then add the tile
+                combinedBehavior.AddTile(tile.Position, zone);
+            }
+        }
+       
+        return combinedLayer;
+    }
+
+    public LBSLayer PopulationLayers()
+    {
+        if (ContextLayers.Count == 0) return null;
+
+        //If there's no population layers, return null
+        var populationLayers = ContextLayers.FindAll(l => l.Behaviours.Any(b => b.GetType().Equals(typeof(PopulationBehaviour))));
+        if (populationLayers.Count == 0) return null;
+
+        //Clone first
+        var combinedLayer = populationLayers.First().Clone() as LBSLayer;
+
+        //Now we check everything
+        foreach (LBSLayer populationLayer in populationLayers)
+        {
+            //Skip the one we just cloned
+            if (populationLayer.Equals(combinedLayer)) continue;
+            //Get behaviors
+            var tempBehavior = populationLayer.Behaviours.Find(b => b.GetType().Equals(typeof(PopulationBehaviour))) as PopulationBehaviour;
+            var combinedBehavior = combinedLayer.Behaviours.Find(b => b.GetType().Equals(typeof(PopulationBehaviour))) as PopulationBehaviour;
+
+            foreach (TileBundleGroup group in tempBehavior.Tilemap)
+            {
+                var tileCheck = new List<TileBundleGroup>();
+                //Check every tile in the group and see if there's anything in their locations.
+                foreach (LBSTile tile in group.TileGroup)
+                {
+                    var foundTile = combinedBehavior.GetTileGroup(tile.Position);
+                    if (foundTile != null)
+                    {
+                        tileCheck.Add(foundTile);
+                    }
+                }
+                //If there's anything in the tileCheck list, we skip. We don't wanna delete anything!
+                if (tileCheck.Count > 0) continue;
+
+                //Lastly, add the tile
+                combinedBehavior.BundleTilemap.AddGroup(group);
+            }
+        }
+
+        return combinedLayer;
     }
 }
