61c34a51ba7e9dc99f7a35f8d08dbb75da1fe8ea
RosenkreuzAlice <74314360+RosenkreuzAlice@users.noreply.github.com>
2025-08-11 17:25:31 -0400

    wip don't merge

--- LevelBuildingSidekick/Assets/ISI Lab/ExampleResources/Controllers/Scripts/Character.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/ExampleResources/Controllers/Scripts/Character.cs
@@ -1,13 +1,17 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 namespace ISILab.Examples
 {
     public class Character : MonoBehaviour
     {
         public float life = 100;
+        //To give it animations
+        public GameObject modelObject;
 
         public void GetDamage(float damage)
         {
@@ -27,6 +31,17 @@ namespace ISILab.Examples
 
         IEnumerator DamageEffect(float time)
         {
+            var renderer = modelObject.GetComponent<Renderer>();
+            List<Material> materials = new List<Material>(renderer.materials);
+
+            foreach (Material material in materials)
+            {
+                material.SetFloat("_BlinkFactor", 1f);
+            }
+            /*if (characterRenderer == null) yield return null;
+            Debug.Log(characterRenderer + " / " + characterRenderer.materials);
+            characterRenderer.material.SetFloat("_BlinkFactor", 1f);
+            */
             //The thing that changes the color should go here!
             yield return new WaitForSeconds(time);
         }
