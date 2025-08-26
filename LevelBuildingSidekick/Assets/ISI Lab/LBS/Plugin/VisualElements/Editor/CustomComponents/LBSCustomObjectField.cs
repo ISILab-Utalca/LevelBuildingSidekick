9aeebb17074c70aee94e20d2a00862745e746176
Nicolás Acevedo <45216823+rothbilly@users.noreply.github.com>
2025-08-26 17:09:37 -0400

    Update ObjectField

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/VisualElements/Editor/CustomComponents/LBSCustomObjectField.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/VisualElements/Editor/CustomComponents/LBSCustomObjectField.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Drawing;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -11,11 +13,90 @@ namespace ISILab.LBS.CustomComponents
         public static readonly string LBSClassName = "lbs-field";
         public static readonly string LBSFieldClassName = "lbs-object-field";
         
+        private VectorImage vectorIcon;
+        private IconPosition iconPosition = IconPosition.Left;
+
+        private VisualElement iconVisualElement;
+        
+        #region Properties
+
+        [UxmlAttribute]
+        public VectorImage VectorIcon
+        {
+            get => vectorIcon;
+            set
+            {
+                vectorIcon = value;
+                if (iconVisualElement != null)
+                {
+                    if (vectorIcon == null)
+                    {
+                        iconVisualElement.style.display = DisplayStyle.None;
+                        return;
+                    }
+                    iconVisualElement.style.display = DisplayStyle.Flex;
+                    iconVisualElement.style.backgroundImage = new StyleBackground(vectorIcon);
+                }
+            }
+        }
+
+        [UxmlAttribute]
+        public IconPosition IconSide
+        {
+            get => iconPosition;
+            set
+            {
+                iconPosition = value;
+                
+                if (iconVisualElement != null)
+                {
+                    switch (iconPosition)
+                    {
+                        case IconPosition.Left:
+                            iconVisualElement.style.display = DisplayStyle.Flex;
+                            iconVisualElement.SendToBack();
+                            break;
+                        case IconPosition.None:
+                            iconVisualElement.style.display = DisplayStyle.None;
+                            break;
+                        case IconPosition.Right:
+                            iconVisualElement.style.display = DisplayStyle.Flex;
+                            iconVisualElement.BringToFront();
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+        }
+
+        #endregion
+        
         public LBSCustomObjectField() : base()
         {
             RemoveFromClassList(ussClassName);
             AddToClassList(LBSClassName);
             AddToClassList(LBSFieldClassName);
+            
+            iconVisualElement = new VisualElement();
+            iconVisualElement.AddToClassList(LBSCustomStyle.LBS_ICON);
+            this.Add(iconVisualElement);
+            if (vectorIcon != null)
+            {
+                iconVisualElement.style.backgroundImage = new StyleBackground(vectorIcon);
+                if (iconPosition == IconPosition.Left)
+                {
+                    iconVisualElement.SendToBack();
+                }
+                else
+                {
+                    iconVisualElement.BringToFront();
+                }
+            }
+            else
+            {
+                iconVisualElement.style.display = DisplayStyle.None;
+            }
         }
     }
     
