da880f64ef8881f35f37f6009833a183a0fad783
Nicolás Acevedo <45216823+rothbilly@users.noreply.github.com>
2025-09-24 15:03:49 -0300

    wfc benchmark

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Editor/Tests/WFCBenchmarkReport.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Editor/Tests/WFCBenchmarkReport.cs
@@ -16,6 +16,9 @@ namespace ISILab.LBS.Tests
         [Test, Performance]
         public void TestMap_5x5()
         {
+            SampleGroup sg_01 = new SampleGroup("Run Time", SampleUnit.Millisecond); 
+            SampleGroup sg_02 = new SampleGroup("GC Call", SampleUnit.Undefined); 
+            
             Measure.Method(() =>
                 {
                     Assert.AreEqual(true, WFCassistant.ExecuteTest(false));
@@ -24,9 +27,57 @@ namespace ISILab.LBS.Tests
                 .MeasurementCount(100)
                 .IterationsPerMeasurement(1)
                 .SetUp(() => SetupWFCTest("27e9e2296bf8411458f727b699a7c0fe"))
+                .GC()
                 .CleanUp(CleanUpWFCTest)
                 .Run();
         }
+        
+        [Test, Performance]
+        public void TestMap_10x10()
+        {
+            Measure.Method(() =>
+                {
+                    Assert.AreEqual(true, WFCassistant.ExecuteTest(false));
+                })
+                .WarmupCount(1)
+                .MeasurementCount(50)
+                .IterationsPerMeasurement(1)
+                .SetUp(() => SetupWFCTest("4da2c1388b315494ab46c235d9727bbe"))
+                .CleanUp(CleanUpWFCTest)
+                .Run();
+        }
+        
+        [Test, Performance]
+        public void TestMap_20x20()
+        {
+            Measure.Method(() =>
+                {
+                    Assert.AreEqual(true, WFCassistant.ExecuteTest(false));
+                })
+                .WarmupCount(0)
+                .MeasurementCount(20)
+                .IterationsPerMeasurement(1)
+                .SetUp(() => SetupWFCTest("0e41f853d3bab334db4db772531db63c"))
+                .CleanUp(CleanUpWFCTest)
+                .Run();
+        }
+        
+        [Test, Performance]
+        [Timeout(3600000)]
+        public void TestMap_40x40()
+        {
+            Measure.Method(() =>
+                {
+                    Assert.AreEqual(true, WFCassistant.ExecuteTest(false));
+                })
+                .WarmupCount(0)
+                .MeasurementCount(10)
+                .IterationsPerMeasurement(1)
+                .SetUp(() => SetupWFCTest("01421564a101235499b4448d3801d494"))
+                .CleanUp(CleanUpWFCTest)
+                .Run();
+        }
+        
         
         [Test, Performance]
         public void TestMap_5x5_SameMap()
@@ -39,10 +90,60 @@ namespace ISILab.LBS.Tests
                 .WarmupCount(1)
                 .MeasurementCount(100)
                 .IterationsPerMeasurement(1)
+                .GC()
                 .Run();
             
             CleanUpWFCTest();
         }
+        
+        [Test, Performance]
+        public void TestMap_10x10_SameMap()
+        {
+            SetupWFCTest("4da2c1388b315494ab46c235d9727bbe");
+            Measure.Method(() =>
+                {
+                    Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
+                })
+                .WarmupCount(1)
+                .MeasurementCount(50)
+                .IterationsPerMeasurement(1)
+                .Run();
+            
+            CleanUpWFCTest();
+        }
+        
+        [Test, Performance]
+        public void TestMap_20x20_SameMap()
+        {
+            SetupWFCTest("0e41f853d3bab334db4db772531db63c");
+            Measure.Method(() =>
+                {
+                    Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
+                })
+                .WarmupCount(0)
+                .MeasurementCount(20)
+                .IterationsPerMeasurement(1)
+                .Run();
+            
+            CleanUpWFCTest();
+        }
+        
+        [Test, Performance]
+        public void TestMap_40x40_SameMap()
+        {
+            SetupWFCTest("01421564a101235499b4448d3801d494");
+            Measure.Method(() =>
+                {
+                    Assert.AreEqual(true, WFCassistant.ExecuteTest(true));
+                })
+                .WarmupCount(0)
+                .MeasurementCount(10)
+                .IterationsPerMeasurement(1)
+                .Run();
+            
+            CleanUpWFCTest();
+        }
+        
 
         private void SetupWFCTest(string _guid)
         {
