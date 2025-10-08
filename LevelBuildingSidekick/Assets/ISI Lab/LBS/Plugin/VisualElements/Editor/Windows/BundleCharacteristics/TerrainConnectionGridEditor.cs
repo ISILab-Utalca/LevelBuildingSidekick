448590c457de2c0be60dc5acb3c67750207b3b71
RosenkreuzAlice <74314360+RosenkreuzAlice@users.noreply.github.com>
2025-10-08 17:37:30 -0300

    don't merge Terrain Connection Grid files added

---
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/VisualElements/Editor/Windows/BundleCharacteristics/TerrainConnectionGridEditor.cs
@@ -0,0 +1,23 @@
+﻿using ISILab.Commons.Utility.Editor;
+using ISILab.LBS.Characteristics;
+using ISILab.LBS.Components;
+using ISILab.LBS.CustomComponents;
+using ISILab.Macros;
+using LBS.Bundles;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using ISILab.LBS.Internal;
+using ISILab.LBS;
+using ISILab.LBS.VisualElements;
+
+public class TerrainConnectionGridEditor : EditorWindow
+{
+    
+
+
+}
