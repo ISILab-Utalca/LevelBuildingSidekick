4ba37c08219b5fa3f057e0e837419154e0f787e4
RosenkreuzAlice <74314360+RosenkreuzAlice@users.noreply.github.com>
2025-10-14 17:39:16 -0300

    don't merge!!!!!!!!

--- LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Components/Characteristics/LBSTerrainConnectionGrid/LBSTerrainConnectionGrid.cs
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/Components/Characteristics/LBSTerrainConnectionGrid/LBSTerrainConnectionGrid.cs
@@ -1,16 +1,69 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using LBS.Bundles;
+using System.Linq;
 
-public class BSTerrainConnectionGrid : MonoBehaviour
+namespace ISILab.LBS.Characteristics
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [System.Serializable]
+    [LBSCharacteristic("Connection Grid", "")]
+    public class LBSTerrainConnectionGrid : LBSCharacteristic, ICloneable
     {
-        
+        public Dictionary<Asset, AssetConnectionGrid> gridList;
+        #region PROPERTIES
+        public List<Asset> Assets
+        {
+            get => Owner.Assets;
+        }
+        #endregion
+
+        #region CONSTRUCTOR
+        public LBSTerrainConnectionGrid() {
+
+        }
+        #endregion
+
+        #region METHODS
+        public override object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override List<string> Validate()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public class AssetConnectionGrid
     {
-        
+        public int[] terrainFlag = new int[9];
+
+        public int[] TerrainFlag
+        {
+            get => terrainFlag;
+        }
+
+        public int VectorToIntConversion(Vector2 vector)
+        {
+            //If over 9, return
+            if ((vector.x * vector.y) > (terrainFlag.Length)) { return -1; }
+
+            throw new NotImplementedException();
+
+        }
     }
 }
+
