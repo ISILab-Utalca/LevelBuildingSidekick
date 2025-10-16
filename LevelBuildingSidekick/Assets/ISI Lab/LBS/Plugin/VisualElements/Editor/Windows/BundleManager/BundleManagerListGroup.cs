84cadfee352d4c6545fc6518fb22a4fc20cdbbf8
Nicolás Acevedo <45216823+rothbilly@users.noreply.github.com>
2025-10-16 17:52:05 -0300

    BundleCustomItemListWithFoldout

---
+++ LevelBuildingSidekick/Assets/ISI Lab/LBS/Plugin/VisualElements/Editor/Windows/BundleManager/BundleManagerListGroup.cs
@@ -0,0 +1,56 @@
+using ISILab.Commons.Utility.Editor;
+using ISILab.LBS.CustomComponents;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace ISI_Lab.LBS.Plugin.VisualElements.Editor.Windows.BundleManager
+{
+    
+    [UxmlElement]
+    public partial class BundleManagerListGroup: VisualElement
+    {
+
+        #region INTERNAL FIELDS
+        private VisualElement titleCard;
+        private Button leftSideButton;
+        private Button rightSideButton;
+        private Label titleLabel;
+        private LBSCustomListView listView;
+        #endregion
+        
+        
+        #region ATRIBUTES
+        [UxmlAttribute]
+        public string TitleText
+        {
+            get => titleLabel.text;
+            set
+            {
+                if (titleLabel != null) titleLabel.text = value;
+            }
+        }
+
+        #endregion
+        
+        
+        public BundleManagerListGroup() : base()
+        {
+            VisualTreeAsset vta = DirectoryTools.GetAssetByName<VisualTreeAsset>(nameof(BundleManagerListGroup));
+            vta?.CloneTree(this);
+            
+            titleCard = this.Q<VisualElement>("TitleCard");
+            leftSideButton = this.Q<Button>("ExpandButton");
+            rightSideButton = this.Q<Button>("NewBundleButton");
+            titleLabel = this.Q<Label>("TitleLabel");
+            listView = this.Q<LBSCustomListView>("List");
+            
+            
+            
+            
+            
+            
+        }
+    }
+    
+}
+
